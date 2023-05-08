655f3f4ae5088575721b57786c52d30e3147f097
dayoff53 <jaehyunlee53529@gmail.com>
2023-05-09 03:37:20 +0900

    Faet : SceneLoadButtonTesting(테스트용 스크립트) 추가

---
+++ Assets/Scripts/Test/SceneLoadButtonTesting.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoadButtonTesting : MonoBehaviour
+{
+    public string loadSceneName;
+    public void Click()
+    {
+        SceneChanger.Instance.SceneLoader(loadSceneName);
+    }
+}
