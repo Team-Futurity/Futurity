24a57ca72d046a67ffe1fdc770597622bc41d55c
Budy123 <kth5948@naver.com>
2023-05-10 16:45:27 +0900

    Chore : Attack 분리

---
+++ Assets/Scripts/Objects/Player/PlayerState/PlayerAttackState_Charged.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[FSMState((int)PlayerController.PlayerState.ChargedAttack)]
+public class PlayerAttackState_Charged : PlayerAttackState
+{
+	public override void Begin(PlayerController pc)
+	{
+		base.Begin(pc);
+	}
+
+	public override void End(PlayerController pc)
+	{
+		base.End(pc);
+	}
+
+	public override void FixedUpdate(PlayerController unit)
+	{
+		base.FixedUpdate(unit);
+	}
+
+	public override void OnTriggerEnter(PlayerController unit, Collider other)
+	{
+		base.OnTriggerEnter(unit, other);
+	}
+
+	public override void Update(PlayerController pc)
+	{
+		base.Update(pc);
+	}
+}
