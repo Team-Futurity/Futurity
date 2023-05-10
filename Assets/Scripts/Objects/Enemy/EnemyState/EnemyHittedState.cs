b5916ab6950c845ded9457e9b7b7949a77da5538
j1nx09 <112684301+j1nx09@users.noreply.github.com>
2023-05-11 01:46:02 +0900

    Feat : Hitted state 구현

--- Assets/Scripts/Objects/Enemy/EnemyState/EnemyHittedState.cs
+++ Assets/Scripts/Objects/Enemy/EnemyState/EnemyHittedState.cs
@@ -7,12 +7,23 @@ public class EnemyHittedState : UnitState<EnemyController>
 {
 	public override void Begin(EnemyController unit)
 	{
-		
+		unit.animator.SetTrigger(unit.hitAnimParam);
+		unit.eMaterial.color = Color.red;
 	}
 
 	public override void Update(EnemyController unit)
 	{
-		
+		unit.hitCurTime += Time.deltaTime;
+
+
+
+		if (unit.hitCurTime > unit.hitMaxTime)
+		{
+			if (!unit.IsCurrentState(EnemyController.EnemyState.Chase))
+			{
+				unit.ChangeState(EnemyController.EnemyState.Chase);
+			}
+		}
 	}
 
 	public override void FixedUpdate(EnemyController unit)
@@ -22,7 +33,8 @@ public class EnemyHittedState : UnitState<EnemyController>
 
 	public override void End(EnemyController unit)
 	{
-		
+		unit.hitCurTime = 0;
+		unit.eMaterial.color = unit.defaultColor;
 	}
 
 	public override void OnTriggerEnter(EnemyController unit, Collider other)
