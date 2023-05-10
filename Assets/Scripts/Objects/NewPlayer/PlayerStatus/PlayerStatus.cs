088ea8e2f6c2b5b22cd9a52dcc971133da64de28
rbase0218 <rbase@naver.com>
2023-05-10 21:44:16 +0900

    Remove : PlayerStatus 제거

--- Assets/Scripts/Objects/NewPlayer/PlayerStatus/PlayerStatus.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PlayerStatus", menuName = "Status/Player")]
-public class PlayerStatus : UnitStatus
-{
-	
-}
