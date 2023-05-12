46f4726da0a083e7fbc599ea2398c3f4d1a1df16
rbase0218 <rbase@naver.com>
2023-05-12 20:28:40 +0900

    Feat : UnitBase의 기존 메서드 추가

--- Assets/Scripts/Objects/Unit/UnitBase.cs
+++ Assets/Scripts/Objects/Unit/UnitBase.cs
@@ -5,5 +5,22 @@ using UnityEngine;
 
 public abstract class UnitBase : MonoBehaviour
 {
+	public UnitStatus status;
 	
+	protected virtual float GetCritical()
+	{
+		return UnityEngine.Random.Range(0, 1) < status.GetStatus(StatusName.CRITICAL_CHANCE) ? status.GetStatus(StatusName.CRITICAL_DAMAGE_MULTIPLIER) : 1;
+	}
+
+	protected abstract float GetAttakPoint(); // 최종 공격력을 반환
+	protected abstract float GetDefensePoint(); // 최종 방어력 반환
+	protected abstract float GetDamage(); // 최종 데미지 반환
+
+	public abstract void Hit(UnitBase attacker, float damage); // Unit이 피격 됐을 때 호출
+	public abstract void Attack(UnitBase target); // Unit이 공격할 때 호출
+
+	public void OnDrawGizmos()
+	{
+		Debug.DrawRay(transform.position, transform.forward, Color.yellow);
+	}
 }
