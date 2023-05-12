c9cf44ca82aaee0bed17f8830af0e319b592957a
rbase0218 <rbase@naver.com>
2023-05-12 20:18:19 +0900

    Feat : UnitStatus 추가

--- Assets/Scripts/Objects/Unit/Status/UnitStatus.cs
+++ Assets/Scripts/Objects/Unit/Status/UnitStatus.cs
@@ -1,11 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
+using System.Linq;
 using UnityEngine;
 
 
 [CreateAssetMenu(fileName ="UnitStatus", menuName = "Status/UnitStatus", order = 0)]
 public class UnitStatus : ScriptableObject
 {
-	public SerializableDictionary<StatusName, float> statusDic = new SerializableDictionary<StatusName, float>();
+	[field: SerializeField] public List<StatusData> Status { get; private set; }
+	[HideInInspector] public List<StatusData> copyStatus;
+
+	private void OnEnable()
+	{
+		copyStatus = Status;
+	}
+
+	public float GetStatus(StatusName name)
+	{
+		return copyStatus.First(x => x.name == name).value;
+	}
+
+	public void SetStatus(StatusName name, float value)
+	{
+		copyStatus.First(x => x.name == name).value = value;
+	}
+
+	// 해당하는 데이터가 있는지 검증한다.
+	private void ValidationStatus(StatusName name)
+	{
+		
+	}
 }
