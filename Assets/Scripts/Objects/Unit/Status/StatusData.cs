cb94756c9711ac89be410ca6435a76bc100e7894
rbase0218 <rbase@naver.com>
2023-05-12 20:18:09 +0900

    Feat : Status Data 추가

---
+++ Assets/Scripts/Objects/Unit/Status/StatusData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class StatusData
+{
+	public StatusName name;
+	public float value;
+}
