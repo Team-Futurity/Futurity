4db945e68c9c421c350e205c8acc199c4c1d0072
rbase0218 <rbase@naver.com>
2023-05-14 01:47:58 +0900

    Fix : Stun 초안 작성

--- Assets/Scripts/System/Buff/Each/BuffStun.cs
+++ Assets/Scripts/System/Buff/Each/BuffStun.cs
@@ -9,11 +9,22 @@ public class BuffStun : BuffBehaviour
 	public override void Active(UnitBase unit)
 	{
 		base.Active(unit);
-		
+
+		targetUnit.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+		if (targetUnit.transform.CompareTag("Player"))
+		{
+			
+		}
+
 	}
 
 	public override void UnActive()
 	{
+		var rig = targetUnit.GetComponent<Rigidbody>();
+		
+		rig.constraints = RigidbodyConstraints.None;
+		rig.constraints = RigidbodyConstraints.FreezeRotation;
 		
 		base.UnActive();
 	}
