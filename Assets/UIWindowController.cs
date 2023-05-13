6a991efc5a472556d0b1b8f1086ab9fdc82a7ca3
dayoff53 <jaehyunlee53529@gmail.com>
2023-05-13 17:04:01 +0900

    Fix : UIWindow를 드레그로 옮길 수 있도록 기능 추가

--- Assets/UIWindowController.cs
+++ Assets/UIWindowController.cs
@@ -6,10 +6,13 @@ using UnityEngine.UIElements;
 
 
 //#설명#	해당 스크립트는 UIWindow가 가지고 있는 핵심 스크립트로, UIWindow의 전반적인 행동 양식을 지니고 있습니다.
-public class UIWindowController : MonoBehaviour, IPointerClickHandler
+public class UIWindowController : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler
 {
-
 	RectTransform rectTransform;
+	//#설명#
+	private Vector2 _pointerPosition;
+	private Vector3 _windowPosition;
+
 
 	public void Start()
 	{
@@ -39,4 +42,15 @@ public class UIWindowController : MonoBehaviour, IPointerClickHandler
 		rectTransform.SetAsLastSibling();
 	}
 
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		_pointerPosition = eventData.position; 
+		_windowPosition = rectTransform.localPosition;
+		BringToFront();
+	}
+	public void OnDrag(PointerEventData eventData)
+	{
+		Vector2 pointerDelta = eventData.position - _pointerPosition;
+		rectTransform.localPosition = _windowPosition + new Vector3(pointerDelta.x, pointerDelta.y, 0);
+	}
 }
