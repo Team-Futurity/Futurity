dddb1b6ffe1dc8cfb9d21b085333719f7b795a3c
dayoff53 <jaehyunlee53529@gmail.com>
2023-05-13 16:27:16 +0900

    Fix : UIManager의 tag찾기 기능을 Dictionary로 관리하도록 변경

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -5,12 +5,27 @@ using UnityEngine.UI;
 
 public class UIManager : Singleton<UIManager>
 {
+	//#설명#	UI저장소
+	private Dictionary<string, List<GameObject>> uiWindows;
+
+
 	//#설명#	UI 창을 인스턴스화하고 부모와 위치를 설정하는 함수
 	public GameObject UIWindowOpen(GameObject OpenUIWindowObject, Transform uiParent, Vector2 instancePosition)
 	{
 		GameObject newUI = Instantiate(OpenUIWindowObject, uiParent);
 		RectTransform rectTransform = newUI.GetComponent<RectTransform>();
 		rectTransform.localPosition = instancePosition;
+
+		if (!string.IsNullOrEmpty(tag))
+		{
+			newUI.tag = tag;
+			if (!uiWindows.ContainsKey(tag))
+			{
+				uiWindows[tag] = new List<GameObject>();
+			}
+			uiWindows[tag].Add(newUI);
+		}
+
 		return newUI;
 	}
 
@@ -26,11 +41,13 @@ public class UIManager : Singleton<UIManager>
 	//#설명#	특정 태그를 가진 UI창을 닫는 함수
 	public void CloseAllWindowsWithTag(string tag)
 	{
-		//#보완#	ObjectPull을 사용하도록 변경해볼것
-		GameObject[] taggedUIWindows = GameObject.FindGameObjectsWithTag(tag);
-		foreach (GameObject taggedUIWindow in taggedUIWindows)
+		if (uiWindows.ContainsKey(tag))
 		{
-			Destroy(taggedUIWindow);
+			foreach (GameObject taggedUIWindow in uiWindows[tag])
+			{
+				Destroy(taggedUIWindow);
+			}
+			uiWindows[tag].Clear();
 		}
 	}
 }
