d8993128f0c2cea5aa78f20fd5602cf79d4999ee
dayoff53 <jaehyunlee53529@gmail.com>
2023-05-13 16:18:25 +0900

    Feat : UIWindowController 추가

---
+++ Assets/UIWindowController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIWindowController : MonoBehaviour
+{
+
+	public void UIWindowNewOpen(GameObject OpenUIWindowObject)
+    {
+		RectTransform rectTransform = GetComponent<RectTransform>();
+
+		UIManager.Instance.UIWindowOpen(OpenUIWindowObject, transform.parent, rectTransform.localPosition + new Vector3(50, -50, 0));
+    }
+
+    public void UIWindowExit()
+    {
+        Destroy(this.gameObject);
+    }
+}
