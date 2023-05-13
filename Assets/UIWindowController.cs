49ea65b09b16c78e4ed4ae6d9bf7759f9b69c453
dayoff53 <jaehyunlee53529@gmail.com>
2023-05-13 17:26:12 +0900

    Refactor : WindowController의 OnBeginDrag기능을 WindowDragController으로 분할

--- Assets/UIWindowController.cs
+++ Assets/UIWindowController.cs
@@ -2,21 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UIElements;
 
 
 //#설명#	해당 스크립트는 UIWindow가 가지고 있는 핵심 스크립트로, UIWindow의 전반적인 행동 양식을 지니고 있습니다.
-public class UIWindowController : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler
+public class UIWindowController : MonoBehaviour, IPointerClickHandler
 {
+	[SerializeField]
 	RectTransform rectTransform;
-	//#설명#
-	private Vector2 _pointerPosition;
-	private Vector3 _windowPosition;
 
 
 	public void Start()
 	{
-		rectTransform = GetComponent<RectTransform>();
+		TryGetComponent<RectTransform>(out rectTransform);
 	}
 
 	public void UIWindowNewOpen(GameObject OpenUIWindowObject)
@@ -40,17 +37,5 @@ public class UIWindowController : MonoBehaviour, IPointerClickHandler, IBeginDra
 	public void BringToFront()
 	{
 		rectTransform.SetAsLastSibling();
-	}
-
-	public void OnBeginDrag(PointerEventData eventData)
-	{
-		_pointerPosition = eventData.position; 
-		_windowPosition = rectTransform.localPosition;
-		BringToFront();
-	}
-	public void OnDrag(PointerEventData eventData)
-	{
-		Vector2 pointerDelta = eventData.position - _pointerPosition;
-		rectTransform.localPosition = _windowPosition + new Vector3(pointerDelta.x, pointerDelta.y, 0);
 	}
 }
