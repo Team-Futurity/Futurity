9a8045c6115f97f6ca4f7f9a2a094b05d5cc1446
dayoff53 <jaehyunlee53529@gmail.com>
2023-05-13 16:19:21 +0900

    Fix : UIManager Singleton으로 변경

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour
+public class UIManager : Singleton<UIManager>
 {
-	[SerializeField] private GameObject uiObject;
-
-	public void CreateUIElementWithWidth(Vector2 uiSize, Transform uiParent, Vector2 instancePosition)
+	//#설명#	UI 창을 인스턴스화하고 부모와 위치를 설정하는 함수
+	public GameObject UIWindowOpen(GameObject OpenUIWindowObject, Transform uiParent, Vector2 instancePosition)
 	{
-		GameObject newUI = Instantiate(uiObject, uiParent);
+		GameObject newUI = Instantiate(OpenUIWindowObject, uiParent);
 		RectTransform rectTransform = newUI.GetComponent<RectTransform>();
-		rectTransform.sizeDelta = new Vector2(uiSize.x, uiSize.y);
 		rectTransform.localPosition = instancePosition;
+		return newUI;
 	}
 }
