34aaba63c66431e4a1c0a80d553283c5be4fa560
dayoff53 <jaehyunlee53529@gmail.com>
2023-05-13 16:28:22 +0900

    Fix : UIManager의 Tag찾기 기능 제거

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -5,26 +5,12 @@ using UnityEngine.UI;
 
 public class UIManager : Singleton<UIManager>
 {
-	//#설명#	UI저장소
-	private Dictionary<string, List<GameObject>> uiWindows;
-
-
 	//#설명#	UI 창을 인스턴스화하고 부모와 위치를 설정하는 함수
 	public GameObject UIWindowOpen(GameObject OpenUIWindowObject, Transform uiParent, Vector2 instancePosition)
 	{
 		GameObject newUI = Instantiate(OpenUIWindowObject, uiParent);
 		RectTransform rectTransform = newUI.GetComponent<RectTransform>();
 		rectTransform.localPosition = instancePosition;
-
-		if (!string.IsNullOrEmpty(tag))
-		{
-			newUI.tag = tag;
-			if (!uiWindows.ContainsKey(tag))
-			{
-				uiWindows[tag] = new List<GameObject>();
-			}
-			uiWindows[tag].Add(newUI);
-		}
 
 		return newUI;
 	}
@@ -35,19 +21,6 @@ public class UIManager : Singleton<UIManager>
 		foreach (Transform child in parentTransform)
 		{
 			Destroy(child.gameObject);
-		}
-	}
-
-	//#설명#	특정 태그를 가진 UI창을 닫는 함수
-	public void CloseAllWindowsWithTag(string tag)
-	{
-		if (uiWindows.ContainsKey(tag))
-		{
-			foreach (GameObject taggedUIWindow in uiWindows[tag])
-			{
-				Destroy(taggedUIWindow);
-			}
-			uiWindows[tag].Clear();
 		}
 	}
 }
