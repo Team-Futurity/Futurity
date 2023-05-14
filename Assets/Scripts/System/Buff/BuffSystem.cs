067b2632302b36895c5b8ea456518fce5c91b5dd
rbase0218 <rbase@naver.com>
2023-05-14 20:09:58 +0900

    Refactor : BuffSystem 테스트용 코드 제거 및 메서드 네이밍 변경

--- Assets/Scripts/System/Buff/BuffSystem.cs
+++ Assets/Scripts/System/Buff/BuffSystem.cs
@@ -5,33 +5,33 @@ using UnityEngine;
 public class BuffSystem : MonoBehaviour
 {
 	// 버프를 추가하는 List
-	[field: SerializeField] public List<BuffBehaviour> BuffList;
+	[field: SerializeField] public List<BuffBehaviour> BuffList { get; private set; }
 	// 버프를 관리하는 Dic
-	private Dictionary<BuffName, BuffBehaviour> buffDic;
+	private Dictionary<BuffNameList, BuffBehaviour> buffDic;
     
-	// Test Code
-    public UnitBase test;
-
     private void Awake()
     {
-		buffDic = new Dictionary<BuffName, BuffBehaviour>();
+		buffDic = new Dictionary<BuffNameList, BuffBehaviour>();
 
 		if(BuffList is not null)
 		{
 			foreach(var buff in BuffList)
 			{
-				buffDic.Add(buff.CurrBuffName, buff);
+				buffDic.Add(buff.BuffData.BuffName, buff);
 			}
 		}
-		
-	    OnBuff(BuffName.SHOCK, test);
     }
-    
-    public void OnBuff(BuffName buffName, UnitBase unit)
-    {
-		var haveBuff = HaveBuff(buffName);
 
-		if (!haveBuff)
+	private bool HasBuff(BuffNameList buffName)
+	{
+		return buffDic.ContainsKey(buffName);
+	}
+
+	public void OnBuff(BuffNameList buffName, UnitBase unit)
+    {
+		var hasBuff = HasBuff(buffName);
+
+		if (!hasBuff)
 		{
 			FDebug.Log($"{buffName}이(가) 존재하지 않습니다;");
 			return;
@@ -47,10 +47,5 @@ public class BuffSystem : MonoBehaviour
 		buffObj.GetComponent<BuffBehaviour>().Active(unit);
 
 		buffObj.gameObject.SetActive(true);
-	}
-    
-	private bool HaveBuff(BuffName buffName)
-	{
-		return buffDic.ContainsKey(buffName);
 	}
 }
