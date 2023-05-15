987a890ed1ca2034716bf9552158bf4a6b57840b
dayoff53 <jaehyunlee53529@gmail.com>
2023-05-15 21:36:37 +0900

    Chore : UnityEvent를 전달하는 작업을 별도의 함수로 따로 할당

--- Assets/Scripts/Test/Test_JH/UIWindowOpenButton.cs
+++ Assets/Scripts/Test/Test_JH/UIWindowOpenButton.cs
@@ -8,6 +8,8 @@ public class UIWindowOpenButton : MonoBehaviour
 	[SerializeField]
 	private GameObject openUiWindow;
 	[SerializeField]
+	private Transform openUiWindowParent;
+	[SerializeField]
 	private Vector2 instancePosition = Vector2.zero;
 	[SerializeField]
 	private Vector2 windowScale = Vector2.one;
@@ -16,7 +18,7 @@ public class UIWindowOpenButton : MonoBehaviour
 	[SerializeField]
 	private UnityEvent[] windowEvents = new UnityEvent[8];
 
-
+	//#설명#	세로운 UI를 인스턴스화 시킨다.
 	public void UiOpenButtonClick()
 	{
 		GameObject instanceUi;
@@ -24,28 +26,42 @@ public class UIWindowOpenButton : MonoBehaviour
 		{
 			if (isWindowLock)
 			{
-				instanceUi = UIWindowManager.Instance.UIWindowOpen(openUiWindow, transform.parent, instancePosition, windowScale);
+				instanceUi = UIWindowManager.Instance.UIWindowTopOpen(openUiWindow, instancePosition, windowScale);
 			}
 			else
 			{
-				instanceUi = UIWindowManager.Instance.UIWindowOpen(openUiWindow, transform.parent, instancePosition, windowScale);
-			}
-
-			UIWindowController windowController = instanceUi.GetComponent<UIWindowController>();
-			windowController.isLock = isWindowLock;
-
-			for (int i = 0; i < windowEvents.Length; i++)
-			{
-				if (windowEvents[i].GetPersistentEventCount() > 0)
+				if (openUiWindowParent)
 				{
-					windowController.windowEvents[i] = windowEvents[i];
-					Debug.Log($"{i}번째 할당 {windowEvents[i]}");
+					instanceUi = UIWindowManager.Instance.UIWindowOpen(openUiWindow, openUiWindowParent, instancePosition, windowScale);
+				}
+				else
+				{
+					instanceUi = UIWindowManager.Instance.UIWindowOpen(openUiWindow, transform.parent, instancePosition, windowScale);
 				}
 			}
+
+			SetUiEvent(instanceUi);
 		}
 		else
 		{
 			FDebug.LogWarning($"{gameObject.name}의 UiOpenButtonClick에 OpenUiWindow가 존재하지 않습니다.");
 		}
 	}
+
+	//#설명#	이벤트 전달
+	private void SetUiEvent(GameObject instanceUi)
+	{
+		UIWindowController windowController = instanceUi.GetComponent<UIWindowController>();
+		windowController.isLock = isWindowLock;
+
+		for (int i = 0; i < windowEvents.Length; i++)
+		{
+			if (windowEvents[i].GetPersistentEventCount() > 0)
+			{
+				windowController.windowEvents[i] = windowEvents[i];
+				FDebug.Log($"{i}번째 할당 {windowEvents[i]}");
+			}
+		}
+	}
+	
 }
