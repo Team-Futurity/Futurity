bbebec9ede54dc8534ab91133a71bcbec9a7e8d6
dayoff53 <jaehyunlee53529@gmail.com>
2023-05-15 18:48:52 +0900

    Feat : SceneChanger의 SceneChgange가 메게변수 기반으로 움직이도록 기능 추가

--- Assets/Scripts/Test/Test_JH/SceneChanger.cs
+++ Assets/Scripts/Test/Test_JH/SceneChanger.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    public string loadSceneName;
-
-	public void SceneChange()
+	public void SceneChange(string loadSceneName)
 	{
 		SceneChangeManager.Instance.SceneLoader(loadSceneName);
     }
+
+	public void ResetScene()
+	{
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(currentSceneIndex);
+	}
 }
