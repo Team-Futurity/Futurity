cb00478a6b2ec4dd0da7680f16344f80b9c07864
dayoff53 <jaehyunlee53529@gmail.com>
2023-05-16 08:46:36 +0900

    Feat : UISelectManager를 UIWindowManager에 병합

--- Assets/Scripts/UI/Window/UIWindowManager.cs
+++ Assets/Scripts/UI/Window/UIWindowManager.cs
@@ -1,12 +1,25 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+using System;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class UIWindowManager : Singleton<UIWindowManager>
 {
+	public InputActionReference leftAction;
+	public InputActionReference rightAction;
+	public InputActionReference selectAction;
+
+	public List<Button> buttons;
+
+	[SerializeField]
+	private int currentButtonIndex;
+	[SerializeField]
+	private GameObject currentButton;
+
 	public GameObject modalBackground;
 	private GameObject parentCanvas;
 	private Transform topCanvasTransform;
@@ -21,21 +34,39 @@ public class UIWindowManager : Singleton<UIWindowManager>
 			image.color = new Color(0, 0, 0, 0.5f);
 		}
 
-
 		topCanvasTransform = FindTopCanvas().transform;
 
 		SceneManager.sceneLoaded += OnSceneLoaded;
+		SelectButton(0);
 	}
 
-	//#설명#	Scene이 로드될 때마다 topCanvasTransform을 다시 설정하기 위해 FindTopCanvas를 호출한다.
+	private void OnDisable()
+	{
+		//#설명#	각 액션의 이벤트 핸들러를 제거합니다.
+
+
+		leftAction.action.performed -= _ => SelectPreviousButton();
+		rightAction.action.performed -= _ => SelectNextButton();
+		selectAction.action.performed -= _ => ClickCurrentButton();
+
+		leftAction.action.Disable();
+		rightAction.action.Disable();
+		selectAction.action.Disable();
+	}
+
 	private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
 	{
+		//#설명#	Scene이 로드될 때마다 topCanvasTransform을 다시 설정하기 위해 FindTopCanvas를 호출한다.
+
+
 		topCanvasTransform = FindTopCanvas().transform;
 	}
 
-	//#설명#	가장 상위 Canvas를 찾아서 topCanvasTransform에 할당함
 	private GameObject FindTopCanvas()
 	{
+		//#설명#	가장 상위 Canvas를 찾아서 topCanvasTransform에 할당함
+
+
 		Canvas[] allCanvases = FindObjectsOfType<Canvas>();
 
 		Canvas topCanvas = null;
@@ -46,23 +77,25 @@ public class UIWindowManager : Singleton<UIWindowManager>
 				topCanvas = canvas;
 			}
 		}
-		
+
 		if (topCanvas != null)
-        {
-            Debug.Log("Top Canvas: " + topCanvas.name);
+		{
+			Debug.Log("Top Canvas: " + topCanvas.name);
 			return topCanvas.gameObject;
 		}
-        else
-        {
-            Debug.Log("No Canvas found.");
+		else
+		{
+			Debug.Log("No Canvas found.");
 			return null;
 		}
 	}
 
 	public GameObject CreateModalBackground(GameObject uiWindow)
 	{
-		parentCanvas = FindParentCanvas(uiWindow);
-		modalBackground.transform.parent = parentCanvas.transform;
+		//#설명#	가장 상위 Canvas에다가 ModalBackground를 생성해서 다른 UI 간섭을 막음
+
+
+		modalBackground.transform.parent = topCanvasTransform;
 
 		RectTransform modalRectTransform = modalBackground.GetComponent<RectTransform>();
 		modalRectTransform.anchorMin = Vector2.zero;
@@ -78,9 +111,14 @@ public class UIWindowManager : Singleton<UIWindowManager>
 		return modalBackground;
 	}
 
-	//#설명#	UI 창을 인스턴스화하고 부모와 위치를 설정하는 함수
+
+
+	#region UIWindowOpenClose
 	public GameObject UIWindowOpen(GameObject OpenUiWindowObject, Transform uiParent, Vector2 instancePosition, Vector2 windowScale)
 	{
+		//#설명#	UI 창을 인스턴스화하고 부모와 위치를 설정하는 함수
+
+
 		GameObject newUI = Instantiate(OpenUiWindowObject, uiParent);
 		if (!newUI.CompareTag("UIWindow"))
 		{
@@ -89,14 +127,15 @@ public class UIWindowManager : Singleton<UIWindowManager>
 		RectTransform rectTransform = newUI.GetComponent<RectTransform>();
 		rectTransform.localPosition = instancePosition;
 		rectTransform.localScale = windowScale;
-		newUI.GetComponent<UIWindowController>().parentObject = uiParent.gameObject;
 
 		return newUI;
 	}
 
-	//#설명#	UI 창을 인스턴스화하되, 부모를 가장 상위 Canvas로 설정한다
 	public GameObject UIWindowTopOpen(GameObject OpenUiWindowObject, Vector2 windowPosition, Vector2 windowScale)
 	{
+		//#설명#	UI 창을 인스턴스화하되, 부모를 가장 상위 Canvas로 설정한다
+
+
 		GameObject newUI = Instantiate(OpenUiWindowObject, topCanvasTransform);
 		if (!newUI.CompareTag("UIWindow"))
 		{
@@ -105,41 +144,79 @@ public class UIWindowManager : Singleton<UIWindowManager>
 		RectTransform rectTransform = newUI.GetComponent<RectTransform>();
 		rectTransform.localPosition = windowPosition;
 		rectTransform.localScale = windowScale;
-		newUI.GetComponent<UIWindowController>().parentObject = topCanvasTransform.gameObject;
 
 		return newUI;
 	}
 
-	//#설명#	모든 자식 UI창을 닫는 함수
 	public void UIWindowChildAllClose(Transform parentTransform)
 	{
+		//#설명#	모든 자식 UI창을 닫는 함수
+
+
 		foreach (Transform child in parentTransform)
 		{
 			if (child.CompareTag("UIWindow"))
 				child.gameObject.GetComponent<UIWindowController>().UIWindowClose();
 		}
 	}
+	#endregion
 
 
-	//#설명#	childObject의 부모 클래스중 가장 가까운 Canvas를 찾는 함수
-	public GameObject FindParentCanvas(GameObject childObject)
+	#region SelectButton
+	public void SetButtons(List<Button> buttons)
 	{
-		Transform parentTransform = childObject.transform;
-
-		while (parentTransform != null)
-		{
-			if (parentTransform.GetComponent<Canvas>() != null)
-			{
-				return parentTransform.gameObject;
-			}
-			parentTransform = parentTransform.parent;
-
-			if (parentTransform == null)
-			{
-				return null;
-			}
-		}
-
-		return null;
+		this.buttons = buttons;
 	}
+
+	public void SetInputActionReference(InputActionReference leftAction, InputActionReference rightAction, InputActionReference selectAction)
+	{
+		//#설명#	각 액션에 대한 이벤트 핸들러를 등록합니다.
+		leftAction.action.performed += _ => SelectPreviousButton();
+		rightAction.action.performed += _ => SelectNextButton();
+		selectAction.action.performed += _ => ClickCurrentButton();
+
+		this.leftAction = leftAction;
+		this.rightAction = rightAction;
+		this.selectAction = selectAction;
+
+		leftAction.action.Enable();
+		rightAction.action.Enable();
+		selectAction.action.Enable();
+	}
+
+	private void SelectPreviousButton()
+	{
+		//#설명#	현재 선택된 버튼이 첫 번째 버튼이 아닌 경우 이전 버튼을 선택합니다.
+		if (currentButtonIndex > 0)
+		{
+			SelectButton(currentButtonIndex - 1);
+		}
+	}
+
+	private void SelectNextButton()
+	{
+		//#설명#	현재 선택된 버튼이 마지막 버튼이 아닌 경우 다음 버튼을 선택합니다.
+		if (currentButtonIndex < buttons.Count - 1)
+		{
+			SelectButton(currentButtonIndex + 1);
+		}
+	}
+
+	private void ClickCurrentButton()
+	{
+		//#설명#	현재 선택된 버튼을 클릭합니다.
+		Button currentButton = buttons[currentButtonIndex];
+		ExecuteEvents.Execute(currentButton.gameObject, new BaseEventData(EventSystem.current), ExecuteEvents.submitHandler);
+	}
+
+	private void SelectButton(int index)
+	{
+		//#설명#	주어진 인덱스의 버튼을 선택합니다.
+		currentButtonIndex = index;
+		currentButton = buttons[currentButtonIndex].gameObject;
+		EventSystem.current.SetSelectedGameObject(currentButton);
+	}
+	#endregion
 }
+
+
