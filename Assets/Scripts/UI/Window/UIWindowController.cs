1ad2438c155fdad19e22d682415f65ffed299114
dayoff53 <jaehyunlee53529@gmail.com>
2023-05-19 05:47:16 +0900

    Remove : 불필요한 modalBackground 제거

--- Assets/Scripts/UI/Window/UIWindowController.cs
+++ Assets/Scripts/UI/Window/UIWindowController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.Events;
+using Unity.VisualScripting;
 
 
 //#설명#	해당 스크립트는 UIWindow가 가지고 있는 핵심 스크립트로, UIWindow의 전반적인 행동 양식을 지니고 있습니다.
@@ -36,12 +37,6 @@ public class UIWindowController : MonoBehaviour, IPointerClickHandler
 		//#설명#	isLock이 true라면 타 UI를 간섭하지 못하도록 막음
 		if (isLock)
 		{
-			modalBackground = UIWindowManager.Instance.CreateModalBackground(gameObject);
-
-			RectTransform modalRectTransform = modalBackground.GetComponent<RectTransform>();
-			modalRectTransform.SetAsLastSibling();
-			modalBackground.GetComponent<Image>().raycastTarget = false;
-
 			BringToFront();
 		}
 
@@ -50,41 +45,42 @@ public class UIWindowController : MonoBehaviour, IPointerClickHandler
 	}
 
 
-	//#설명#	태스트용 스크립트로 새로운 Window창을 연다.
 	public void UIWindowNewOpen(GameObject OpenUIWindowObject)
 	{
+		//#설명#	태스트용 스크립트로 새로운 Window창을 연다.
 		UIWindowManager.Instance.UIWindowOpen(OpenUIWindowObject, transform.parent, rectTransform.localPosition + new Vector3(50, -50, 0), rectTransform.sizeDelta);
 	}
 
-	//#설명#	자기 자신을 닫는다.
 	public void UIWindowClose()
 	{
+		//#설명#	자기 자신을 닫는다.
 		UIWindowManager.Instance.UIWindowClose(this.gameObject);
 		closeEvent?.Invoke();
 	}
 
-	//#설명#	형제 객체들을 전부 닫아버린다.
 	public void UIWindowSiblingAllClose()
 	{
+		//#설명#	형제 객체들을 전부 닫아버린다.
 		UIWindowManager.Instance.UIWindowChildAllClose(transform.parent);
 	}
 
-	//#설명#	해당 UIWindow가 클릭되었을때 가장 앞으로 끌고오는 역할
 	public void OnPointerClick(PointerEventData eventData)
 	{
+		//#설명#	해당 UIWindow가 클릭되었을때 가장 앞으로 끌고오는 역할
 		BringToFront();
 	}
 
-	//#설명#	해당 UIWindow를 가장 앞으로 끌고오는 역할
 	public void BringToFront()
 	{
+		//#설명#	해당 UIWindow를 가장 앞으로 끌고오는 역할
 		rectTransform.SetAsLastSibling();
 	}
 
-	//#설명#	이벤트 호출기
 	public void EventStarter(int eventNumber)
 	{
+		//#설명#	이벤트 호출기
 		windowEvents[eventNumber]?.Invoke();
+		UIWindowClose();
 	}
 
 	public List<Button> GetButtons()
