52a09e872c5b0b20e92d90bd64eb96d4ab24c523
rbase0218 <rbase@naver.com>
2023-05-19 04:39:07 +0900

    Feat : Part Enum Data 설정

---
+++ Assets/Scripts/System/Part/Enum/PartEnum.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+
+
+// Common
+
+
+
+
+// Passive
+public enum PassivePartGrade
+{
+	NONE = 0,
+
+	NORMAL,
+	RANK_C,
+	RANK_B,
+	RANK_A,
+
+	MAX
+}
+
+public enum PassiveAbility
+{
+	NONE = 0,
+
+	ATTACK_HEALTH = 11,
+	ATTACK_SHOCK,
+
+	DEFENSE_UP = 21,
+	CRITICAL_UP,
+	ATTACK_UP,
+
+	MAX
+}
+
+
+// Active
