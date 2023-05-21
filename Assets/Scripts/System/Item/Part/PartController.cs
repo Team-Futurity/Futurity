cea9783de04b2c559a1649fd5f2b3b4b54fb5df3
rbase0218 <rbase@naver.com>
2023-05-21 23:03:01 +0900

    Feat : 파츠 장착 구현

--- Assets/Scripts/System/Item/Part/PartController.cs
+++ Assets/Scripts/System/Item/Part/PartController.cs
@@ -6,12 +6,66 @@ public class PartController : MonoBehaviour
 {
 	// List Part
 	public List<Part> equipPart = new List<Part>();
-
+	public int equipCount = 0;
 
 	// Calc
 	public OriginStatus status;
 
+	public void Awake()
+	{
+		status = ScriptableObject.CreateInstance<OriginStatus>();
+		status.AutoGenerator();
 
-	// Part Add
-	// Part Remove -> Dissambly
+		//PassiveEquip(equipPart[0]);
+	}
+
+	public void EquipPart(Part part)
+	{
+		if (equipPart.Count >= equipCount)
+		{
+			return;
+		}
+
+		equipPart.Add(part);
+
+		switch (part.PartItemData.PartType)
+		{
+			case PartTriggerType.PASSIVE:
+				PassiveEquip(part);
+				break;
+
+			case PartTriggerType.ACTIVE:
+				ActiveEquip(part);
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	public void UnequipPart(Part part)
+	{
+		// Find -> Remove
+		// UnEquip Process Start
+	}
+
+	private void PassiveEquip(Part part)
+	{
+		IPassive passivePart;
+
+		part.TryGetComponent(out passivePart);
+
+		if(passivePart is null)
+		{
+			FDebug.Log($"{part.GetType()}이(가) 존재하지 않습니다.");
+			return;
+		}
+
+		passivePart.Active(status);
+	}
+
+	private void ActiveEquip(Part part)
+	{
+
+	}
 }
