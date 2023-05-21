e5178d6995896e81eaee01b4d80ccda8f7f58812
Budy123 <kth5948@naver.com>
2023-05-22 02:14:53 +0900

    Add : 콤보 게이지 관리 시스템

---
+++ Assets/Scripts/Objects/Player/PlayerAttack/ComboGaugeSystem.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[Serializable]
+public class ComboCountData
+{
+	[field: SerializeField] public int ComboCount { get; private set; }
+	[field: SerializeField] public int AddedGauge { get; private set; }
+
+	public ComboCountData(int comboCount, int addedGauge = 0)
+	{
+		ComboCount = comboCount;
+		AddedGauge = addedGauge;
+	}
+}
+
+public class ComboGaugeSystem : MonoBehaviour
+{
+	public int minComboCount = 0;
+	public int maxComboCount = 3;
+	public int minComboGauge = 0;
+	public int maxComboGauge = 100;
+	private int comboCount;
+	private int currentGauge;
+	private int maxGauge;
+	public List<ComboCountData> comboData;
+
+	public int ComboCount { get { return comboCount; } }
+	public int CurrentGauge { get { return currentGauge;} }
+
+	// ComboCount를 결정
+	private void SetComboCount(bool isSucceed)
+	{
+		comboCount = isSucceed ? comboCount-1 : 0;
+
+		if(comboCount == minComboCount - 1)
+		{
+			comboCount = maxComboCount;
+		}
+	}
+
+	// SetComboCount()에서 결정된 comboCount를 기반으로 쌓일 게이지 연산 
+	private int CalculateCurrentGauge(int hittedEnemyCount)
+	{
+		int addedGauge = -1;
+		foreach(ComboCountData data in comboData)
+		{
+			if(data.ComboCount == comboCount) { addedGauge = data.AddedGauge; break; }
+		}
+
+		if(addedGauge == -1)
+		{
+			FDebug.LogError("[ComboGaugeSystem]Cannot Found ComboCountData");
+			return -1;
+		}
+
+		return (int)(addedGauge + Mathf.Sqrt(addedGauge) * hittedEnemyCount);
+	}
+
+	public void SetComboGaugeProc(bool isSucceed, int hittedEnemyCount)
+	{
+		SetComboCount(isSucceed);
+
+		if(!isSucceed || hittedEnemyCount == 0) { return; }
+
+		int addedComboGauge = CalculateCurrentGauge(hittedEnemyCount);
+
+		currentGauge = Mathf.Clamp(currentGauge + addedComboGauge, minComboGauge, maxComboGauge);
+	}
+}
