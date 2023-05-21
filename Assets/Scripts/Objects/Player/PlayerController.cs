9a2711a746a9f446c4a448a22fc9128a8b707a68
Budy123 <kth5948@naver.com>
2023-05-22 00:19:46 +0900

    Style : PlayerController 인스펙터 정리

--- Assets/Scripts/Objects/Player/PlayerController.cs
+++ Assets/Scripts/Objects/Player/PlayerController.cs
@@ -38,43 +38,65 @@ public class PlayerController : UnitFSM<PlayerController>, IFSM
 	public readonly string IsAttackingAnimKey = "IsAttacking";
 	public const int NullState = -1;
 
-	// reference
-	public Player playerData;
-	[HideInInspector] public Animator animator;
-	[HideInInspector] public Rigidbody rigid;
-	[HideInInspector] public TrailRenderer dashEffect;
-	[HideInInspector] public CapsuleCollider basicCollider;
-
-	// move
-	//public Vector3 moveInput;
-	public Vector3 moveDir;
+	[Header("[수치 조절]────────────────────────────────────────────────────────────────────────────────────────────")]
 
 	// attack
+	[Space(2f)]
+	[Header("콤보")]
+	public Tree comboTree;
+
+	[Space(15)]
+	[Header("[디버깅용]─────────────────────────────────────────────────────────────────────────────────────────────")]
+
+	// move
+	[Space(2)]
+	[Header("이동 관련")]
+	public Vector3 moveDir;
+
+	// input
+	[Space(5)]
+	[Header("입력 관련")]
+	public bool specialIsReleased = false;
+
+	// attack
+	[Space(5)]
+	[Header("공격 관련")]
 	public PlayerInput curCombo;
 	public PlayerInput nextCombo;
 	public AttackNode curNode;
-	public Tree comboTree;
+	public PlayerState currentAttackState;
+	[HideInInspector] public string currentAttackAnimKey;
+
+	[Space(15)]
+	[Header("[최초 1회 할당]──────────────────────────────────────────────────────────────────────────────────────────")]
+
+	// reference
+	[Space(2)]
+	[Header("References")]
+	public GameObject glove;
+	public Player playerData;
 	public RadiusCapsuleCollider attackCollider;
 	public RadiusCapsuleCollider autoTargetCollider;
-	public PlayerState currentAttackState;
-	public string currentAttackAnimKey;
+	[HideInInspector] public CapsuleCollider basicCollider;
+	[HideInInspector] public Animator animator;
+	[HideInInspector] public Rigidbody rigid;
+	[HideInInspector] public TrailRenderer dashEffect;
 
-	// input
-	public bool specialIsReleased = false;
-
-	//임시
-	public GameObject glove;
 	[Serializable]
 	public struct EffectData
 	{
 		public Transform effectPos;
 		public GameObject effect;
 	}
+	[Space(5)]
+	[Header("돌진 이펙트")]
 	public List<EffectData> rushEffects;
 	public ObjectPoolManager<Transform> rushObjectPool;
 	public ObjectPoolManager<Transform> rushObjectPool2;
 
 	// sound 
+	[Space(5)]
+	[Header("사운드")]
 	public FMODUnity.EventReference dash;
 	public FMODUnity.EventReference hitMelee;
 	public FMODUnity.EventReference hitRanged;
