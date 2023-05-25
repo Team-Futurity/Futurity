30a080fb8c1ce4fae9591f2da8e20d6facd3f975
dayoff53 <42767781+dayoff53@users.noreply.github.com>
2023-05-25 17:42:25 +0900

    Chore : TempTextManager에서 Combo값을 줄떄 딜레이를 추가, gaugeBarConroller를 배열로 받음

--- Assets/Scripts/UI/TempTestManager.cs
+++ Assets/Scripts/UI/TempTestManager.cs
@@ -7,13 +7,7 @@ using UnityEngine.Events;
 public class TempTestManager : MonoBehaviour
 {
 	[SerializeField]
-	GaugeBarController comboGuageBarController;
-	[SerializeField]
-	GaugeBarController hpGuageBarController;
-	[SerializeField]
-	GaugeBarController bossHpGuageBarController;
-	[SerializeField]
-	GaugeBarController enemyGauageBarrController;
+	List<GaugeBarController> guageBarControllers;
 
 	float targetGaugeValue;
 	bool isValueReached = true;
@@ -32,8 +26,9 @@ public class TempTestManager : MonoBehaviour
 
 	void Start()
 	{
-		TextValueChanger();
+		TextValueSetter();
 		scrapUIController.SetScrapValue(scrapValue);
+		StartCoroutine(NumberImageValueChanger());
 	}
 
 
@@ -47,39 +42,52 @@ public class TempTestManager : MonoBehaviour
 
 	void ComboGuageBarValueChanger()
 	{
-		float gaugeBarValue = comboGuageBarController.gaugeBar.fillAmount;
+		float gaugeBarValue = guageBarControllers[0].gaugeBar.fillAmount;
 
 		if (Mathf.Abs(gaugeBarValue - targetGaugeValue) <= 0.01f)
 		{
 			isValueReached = true;
 		}
 
+		
+
 		if (isValueReached)
 		{
 			targetGaugeValue = Random.Range(0f, 1f);
-			comboGuageBarController.SetGaugeFillAmount(targetGaugeValue);
-			hpGuageBarController.SetGaugeFillAmount(targetGaugeValue);
-			bossHpGuageBarController.SetGaugeFillAmount(targetGaugeValue);
-			enemyGauageBarrController.SetGaugeFillAmount(targetGaugeValue);
-			isValueReached = false;
+
+			for (int i = 0; i < guageBarControllers.Count; i++)
+			{
+				guageBarControllers[i].SetGaugeFillAmount(targetGaugeValue);
+				isValueReached = false;
+
+				Debug.Log($"{guageBarControllers[i].name} : {guageBarControllers[0].GetGaugeIntegerPercent()}");
+			}
 		}
-
-		Debug.Log($"GetGaugeIntegerPercent : {comboGuageBarController.GetGaugeIntegerPercent()}");
-		Debug.Log($"hpGuageBarController : {hpGuageBarController.GetGaugeIntegerPercent()}");
-		Debug.Log($"bossHpGuageBarController : {bossHpGuageBarController.GetGaugeIntegerPercent()}");
-		Debug.Log($"enemyGauageBarrController : {enemyGauageBarrController.GetGaugeIntegerPercent()}");
 	}
 
 
-
-	void NumberImageValueChanger()
+	IEnumerator NumberImageValueChanger()
 	{
-		int randValue = Random.Range(0, 999);
+		int comboDelayCount = 0;
+		while (true) // 무한 반복
+		{
+			int randValue = Random.Range(0, 999);
+			numberImageLoader.SetNumber(randValue);
 
-		numberImageLoader.SetNumber(randValue);
+			if (comboDelayCount < 5)
+			{
+				yield return new WaitForSeconds(1f);
+				comboDelayCount++;
+			}
+			else
+			{
+				yield return new WaitForSeconds(8f);
+				comboDelayCount = 0;
+			}
+		}
 	}
 
-	void TextValueChanger()
+	void TextValueSetter()
 	{
 		characterDialogController.SetCharactorText($"동해물과 백두산이 마르고 닳도록\r\n하느님이 보우하사 우리나라 만세\r\n무궁화 삼천리 화려강산 대한사람 대한으로 길이\r\n보전하세. 남산 위에 저 소나무 철갑을 두른 듯\r\n바람서리 불변함은 우리기상 일세\r\n무궁화 삼천리 화려강산 대한사람 대한으로 길이보전하세 \r\n");
 	}
