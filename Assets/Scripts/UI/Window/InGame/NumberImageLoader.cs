e76eca1df0ef7c82d6af70ee1c39561ad6f1e27c
dayoff53 <42767781+dayoff53@users.noreply.github.com>
2023-05-25 17:42:48 +0900

    Feat : 콤보 값 변경시 추가 이펙트 출력

--- Assets/Scripts/UI/Window/InGame/NumberImageLoader.cs
+++ Assets/Scripts/UI/Window/InGame/NumberImageLoader.cs
@@ -7,21 +7,31 @@ using UnityEngine.UI;
 public class NumberImageLoader : MonoBehaviour
 {
 	[SerializeField]
-	List<Image> numberLoadField;
+	List<Material> numberComboMaterial;
 	[SerializeField]
-	List<Sprite> numberSprite;
+	List<ParticleSystem> numberComboParticle;
+
+	int[] currntCombos = new int[] {0,0,0 };
+
+	[SerializeField]
+	List<Texture> numberSprite;
 
 
 	public void SetNumber(int num)
 	{
-		int i1 = num % 10;
-		int i10 = (num / 10) % 10;
-		int i100 = num / 100;
+		currntCombos[0] = num % 10;
+		currntCombos[1] = (num / 10) % 10;
+		currntCombos[2] = num / 100;
 
-		Debug.Log($"{gameObject.name}ÀÇ SetNumber : {i1}{i10}{i100}");
+		Debug.Log($"{gameObject.name}ÀÇ SetNumber : {currntCombos[0]}{currntCombos[1]}{currntCombos[2]}");
 
-		numberLoadField[0].sprite = numberSprite[i1];
-		numberLoadField[1].sprite = numberSprite[i10];
-		numberLoadField[2].sprite = numberSprite[i100];
+		for (int i = 0; i < 3; i++)
+		{
+			numberComboMaterial[i].SetTexture("_maintex", numberSprite[currntCombos[i]]);
+			numberComboParticle[i].Stop();
+			numberComboParticle[i].Play();
+
+			Debug.Log($"{numberComboMaterial[i].name}ÀÇ mainTexture : {numberComboMaterial[i].GetTexture("_maintex")}");
+		}
 	}
 }
