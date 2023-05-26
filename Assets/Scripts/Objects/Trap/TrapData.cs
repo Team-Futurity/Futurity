360732494def42ea3987a9a0b1a0269e72399a80
rbase0218 <rbase@naver.com>
2023-05-27 00:52:33 +0900

    FIx : UpTrap 초안 작성

--- Assets/Scripts/Objects/Trap/TrapData.cs
+++ Assets/Scripts/Objects/Trap/TrapData.cs
@@ -5,54 +5,40 @@ using UnityEngine;
 [System.Serializable]
 public class TrapData
 {
-	// Trap Code - Not Unique
-	// 함정 오브젝트의 고유 코드
-	public int code;
-
 	// Trap Code - Unique 
 	// 함정 종류마다 가지고 있는 각자의 고유 코드
-	public string uID;
+	// 추후 추가 가능성이 있다고 판단하여 선언만 진행함
+	[field: SerializeField] public string TrapUID { get; private set; }
+	
+	
+	// Trap Code - Not Unique
+	// 함정 오브젝트의 고유 코드
+	[field: SerializeField] public int TrapCode { get; private set; }
 
 	// String
 	// 함정 이름
-	public string name;
+	[field: SerializeField] public string TrapName { get; private set; }
 
 	// Int
 	// 함정의 발동 조건 ( 0 - NONE, 1 - 플레이어 접근, 2 - 플레이어 공격 )
-	public TrapCondition condition;
+	[field: SerializeField] public TrapCondition TrapCondition { get; private set; } 
+	
 	// 함정의 타입 ( 0 - NONE, 1 - FALL, 2 - Debuff, 3 - Explosion )
-	public TrapType type;
+	[field: SerializeField] public TrapType TrapType { get; private set; }
+
 	// 함정 효과 ( 0 - NONE, 1 - Stun, 3 - Damage )
-	public int debuff;
+	[field: SerializeField] public int TrapDebuff { get; private set; }
 
 	// Float Data
 	// 함정 피격 범위
-	public float range;
+	[field: SerializeField] public float TrapRange { get; private set; }
+	
 	// 함정 데미지
-	public float damage;
+	[field: SerializeField] public float TrapDamage { get; private set; }
+	
 	// 함정 상태 이상 시간
-	public float duration;
+	[field: SerializeField] public float TrapDuration { get; private set; }
+	
 	// 함정 쿨타임
-	public float cooldowns;
-}
-
-public enum TrapCondition
-{
-	NONE = 0,
-	
-	IN,
-	ATTACK,
-	
-	MAX
-}
-
-public enum TrapType
-{
-	NONE = 0,
-	
-	FALL,
-	Debuff,
-	Explosion,
-	
-	MAX
+	[field: SerializeField] public float TrapCooldowns { get; private set; }
 }
---
+++ Assets/Scripts/Objects/Trap/UpTrap.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpTrap : TrapBehaviour
+{
+	public override void ActiveTrap(UnitBase[] units)
+	{
+		
+	}
+
+}
