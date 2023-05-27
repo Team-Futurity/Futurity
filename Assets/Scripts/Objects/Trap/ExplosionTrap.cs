181de1b2332aa746553ceb1f85f0b36e1d3de00f
rbase0218 <rbase@naver.com>
2023-05-27 23:19:40 +0900

    Fix : Player 은닉화 적용

--- Assets/Scripts/Objects/Trap/ExplosionTrap.cs
+++ Assets/Scripts/Objects/Trap/ExplosionTrap.cs
@@ -7,10 +7,6 @@ public class ExplosionTrap : TrapBehaviour
 {
 	private List<UnitBase> detectList;
 
-	private UnitBase trapUnit;
-	// 해당 방식은 공격을 했을 경우, 발동이므로 조건에 대해서 명확해야함.
-	// 탐색 범위 만큼 탐색 후, 적들을 탐색하고 해당 적들에게 데미지 부여
-
 	private void Awake()
 	{
 		TryGetComponent(out trapUnit);
@@ -31,12 +27,11 @@ public class ExplosionTrap : TrapBehaviour
 			unit.Hit(trapUnit, 0);
 		}
 		
-		// DestroyTrap();
 	}
 
 	private void SearchAround()
 	{
-		// 해당 메서드에서 주변 유닛 탐색
+		
 	}
 
 	private void DestroyTrap()
