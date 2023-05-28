302f3693036b3bc5304844dad878943f5db625f2
j1nx09 <112684301+j1nx09@users.noreply.github.com>
2023-05-28 23:53:35 +0900

    Chore : 씬 수정

--- Assets/CharacterWindowOpener.cs
+++ Assets/CharacterWindowOpener.cs
@@ -1,26 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class CharacterWindowOpener : MonoBehaviour
 {
-	[SerializeField] 
-	private InputActionReference charactorInput;
-
 	[SerializeField]
-	private GameObject characterWindow;
-
+	private WindowOpenController windowOpenController;
 
 	private void Start()
 	{
-		charactorInput.action.performed += _ => CharacterWindowOpen();
-
-		charactorInput.action.Enable();
-	}
-
-	private void CharacterWindowOpen()
-	{
-		Instantiate(characterWindow);
+		windowOpenController.WindowOpen();
 	}
 }
