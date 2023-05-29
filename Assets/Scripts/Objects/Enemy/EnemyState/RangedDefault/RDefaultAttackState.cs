9827f8fff4a2ce4bca4b253db4da28225bb4b2c0
j1nx09 <112684301+j1nx09@users.noreply.github.com>
2023-05-29 19:27:26 +0900

    Chore : ranged effect, melee animation

--- Assets/Scripts/Objects/Enemy/EnemyState/RangedDefault/RDefaultAttackState.cs
+++ Assets/Scripts/Objects/Enemy/EnemyState/RangedDefault/RDefaultAttackState.cs
@@ -7,14 +7,17 @@ using UnityEngine.ProBuilder;
 public class RDefaultAttackState : UnitState<EnemyController>
 {
 	private float curTime = .0f;
+	private float attackTime = 0.6f;
+	private bool isAttackDone = false;
 	private float distance = .0f;
+	private Vector3 projectilePos = new Vector3(0f, 0.75f, 0f);
 
 	public override void Begin(EnemyController unit)
 	{
 		//FDebug.Log("RDefault attack Begin");
 		unit.animator.SetTrigger(unit.atkAnimParam);
-		unit.rangedProjectile.transform.position = unit.transform.position;
-		unit.rangedProjectile.SetActive(true);
+		unit.rangedProjectile.transform.position = unit.transform.position + projectilePos;
+		/*unit.rangedProjectile.SetActive(true);*/
 	}
 
 	public override void Update(EnemyController unit)
@@ -22,6 +25,12 @@ public class RDefaultAttackState : UnitState<EnemyController>
 		distance = Vector3.Distance(unit.transform.position, unit.rangedProjectile.transform.position);
 		curTime += Time.deltaTime;
 		
+		if(curTime > attackTime && !isAttackDone)
+		{
+			unit.rangedProjectile.SetActive(true);
+			isAttackDone = true;
+		}
+
 		if (distance > unit.projectileDistance)
 			unit.rangedProjectile.SetActive(false);
 
@@ -38,6 +47,7 @@ public class RDefaultAttackState : UnitState<EnemyController>
 		//FDebug.Log("RDefault attack End");
 		unit.rangedProjectile.SetActive(false);
 		curTime = 0f;
+		isAttackDone = false;
 	}
 
 	public override void OnTriggerEnter(EnemyController unit, Collider other)
