9cae3a68143d53fdbfbd14e5a7d8ec7521bccb38
rbase0218 <rbase@naver.com>
2023-06-03 18:13:18 +0900

    Fix : BuffSystem 구현, Active Buffer 별도 관리 진행

--- Assets/Scripts/System/Buff/BuffSystem.cs
+++ Assets/Scripts/System/Buff/BuffSystem.cs
@@ -11,4 +11,38 @@ public class BuffSystem : MonoBehaviour
 		activeBuffDic = new Dictionary<int, BuffBehaviour>();
     }
 
+	public void AddBuff(BuffBehaviour buff)
+	{
+		if(buff is null)
+		{
+			FDebug.Log("[BuffSystem] 해당 버프는 존재하지 않습니다.");
+			return;
+		}
+
+		AddBuffer(buff);
+	}
+
+	public void RemoveBuff(int buffCode)
+	{
+		var hasBuff = HasBuff(buffCode);
+
+		if(!hasBuff)
+		{
+			FDebug.Log("[BuffSystem] 해당 버프는 존재하지 않습니다.");
+			return;
+		}
+
+		activeBuffDic.Remove(buffCode);
+	}
+
+	private void AddBuffer(BuffBehaviour buff)
+	{
+		var uID = buff.BuffData.BuffCode;
+		activeBuffDic.Add(uID, buff);	
+	}
+
+	private bool HasBuff(int buffCode)
+	{
+		return activeBuffDic.ContainsKey(buffCode);
+	}
 }
