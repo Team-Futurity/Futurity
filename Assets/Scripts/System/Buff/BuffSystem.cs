f4c5d961158412b597dee0cdc290e024a1648aee
rbase0218 <rbase@naver.com>
2023-06-03 17:22:48 +0900

    Refactor : BuffSystem 스크립트 용도 변경

--- Assets/Scripts/System/Buff/BuffSystem.cs
+++ Assets/Scripts/System/Buff/BuffSystem.cs
@@ -4,69 +4,11 @@ using UnityEngine;
 
 public class BuffSystem : MonoBehaviour
 {
-	[field: SerializeField] public List<BuffBehaviour> BuffList { get; private set; }
-
-	private Dictionary<int, BuffBehaviour> buffSaveDic;
-	private Dictionary<int, BuffBehaviour> currActiveBuffDic;
+	private Dictionary<int, BuffBehaviour> activeBuffDic;
 
     private void Awake()
     {
-		buffSaveDic = new Dictionary<int, BuffBehaviour>();
-		currActiveBuffDic = new Dictionary<int, BuffBehaviour>();
-
-		SaveBuff();
+		activeBuffDic = new Dictionary<int, BuffBehaviour>();
     }
 
-	// 해당 버프가 있는지 확인한다.
-	public bool HasSaveBuff(int buffCode)
-	{
-		return buffSaveDic.ContainsKey(buffCode);
-	}
-
-	public BuffBehaviour GetSaveBuff(int buffCode)
-	{
-		return buffSaveDic[buffCode];
-	}
-
-	// 버프를 적용한다.
-	public void SetActiveBuff(BuffBehaviour buff)
-    {
-		if(buff is null)
-		{
-			FDebug.Log($"{buff}이(가) 존재하지 않습니다.");
-			return;
-		}
-
-		// Buff Object 생성
-		Instantiate(buff, transform.position, Quaternion.identity, transform);
-		currActiveBuffDic.Add(buff.BuffData.BuffCode, buff);
-	}
-
-	public void RemoveActiveBuff(int buffCode)
-	{
-		var hasActiveBuff = GetActiveBuff(buffCode);
-
-		if (hasActiveBuff)
-		{
-			currActiveBuffDic.Remove(buffCode);
-		}
-	}
-
-	private bool GetActiveBuff(int buffCode)
-	{
-		return currActiveBuffDic.ContainsKey(buffCode);
-	}
-
-	private void SaveBuff()
-	{
-		if(BuffList is null)
-		{
-			return;
-		}
-
-		foreach(var buff in BuffList)
-		{
-			buffSaveDic.Add(buff.BuffData.BuffCode, buff);
-		}
-	}
 }
