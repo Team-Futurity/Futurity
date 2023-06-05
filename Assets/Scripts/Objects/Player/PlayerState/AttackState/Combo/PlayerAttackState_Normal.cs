c841d617c6acf57eda08d74107ff34e1d177eac4
Budy123 <kth5948@naver.com>
2023-06-06 08:18:33 +0900

    Fix : State 관련 폴더 정리2(이동한 파일을 적용 안해서 추가)

---
+++ Assets/Scripts/Objects/Player/PlayerState/AttackState/Combo/PlayerAttackState_Normal.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[FSMState((int)PlayerController.PlayerState.NormalAttack)]
+public class PlayerAttackState_Normal : PlayerAttackState
+{
+	public PlayerAttackState_Normal() : base("ComboTrigger", "Combo") { }
+
+	public override void Begin(PlayerController pc)
+	{
+		base.Begin(pc);
+
+		pc.attackCollider.radiusCollider.enabled = true;
+		pc.attackCollider.SetCollider(attackNode.attackAngle, attackNode.attackLength * PlayerController.cm2m);
+	}
+
+	public override void End(PlayerController pc)
+	{
+		base.End(pc);
+	}
+
+	public override void FixedUpdate(PlayerController unit)
+	{
+		base.FixedUpdate(unit);
+	}
+
+	public override void OnTriggerEnter(PlayerController unit, Collider other)
+	{
+		base.OnTriggerEnter(unit, other);
+	}
+
+	public override void Update(PlayerController pc)
+	{
+		base.Update(pc);
+	}
+}
