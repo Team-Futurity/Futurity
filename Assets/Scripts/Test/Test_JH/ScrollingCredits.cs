f8a4f68219952d1a8fd229a8d8c44830edd9e3fc
dayoff53 <jaehyunlee53529@gmail.com>
2023-06-08 17:01:30 +0900

    Fix : 타 Scene에서 이동시 정상적으로 작동이 안되던 스크립트들 수정

--- Assets/Scripts/Test/Test_JH/ScrollingCredits.cs
+++ Assets/Scripts/Test/Test_JH/ScrollingCredits.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class CraditScrolling : MonoBehaviour
+public class ScrollingCredits : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float speed = 50f;
+	public GameObject textPrefab;
+	public List<string> credits = new List<string>();
+	public Transform startPoint;
+	public UnityEvent OnCreditsEnd;
+	public float cutOffPoint = 600f;
+	public float minimumSpacing = 100f;
+	private List<GameObject> textObjects = new List<GameObject>();
+	[SerializeField]
+	private bool allTextsProcessed = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Start()
+	{
+		allTextsProcessed = false;
+		CreateCredits();
+	}
+
+	void CreateCredits()
+	{
+		Vector3 currentPos = startPoint.position;
+		foreach (string credit in credits)
+		{
+			GameObject textObject = Instantiate(textPrefab, currentPos, Quaternion.identity, startPoint);
+			textObject.GetComponent<TextMeshProUGUI>().text = credit;
+			textObjects.Add(textObject);
+			currentPos -= new Vector3(0, minimumSpacing, 0);
+		}
+	}
+
+	void Update()
+	{
+		for (int i = 0; i < textObjects.Count; i++)
+		{
+			GameObject currentText = textObjects[i];
+			if (i > 0)
+			{
+				GameObject previousText = textObjects[i - 1];
+				if (Vector3.Distance(previousText.transform.localPosition, currentText.transform.localPosition) < minimumSpacing)
+				{
+					continue;
+				}
+			}
+			currentText.transform.Translate(Vector3.up * speed * Time.deltaTime);
+			if (currentText.transform.localPosition.y > cutOffPoint)
+			{
+				Destroy(currentText);
+				textObjects.RemoveAt(i);
+				i--;
+			}
+		}
+		if (textObjects.Count == 0 && !allTextsProcessed)
+		{
+			FDebug.Log("CraditEnd");
+			allTextsProcessed = true;
+			OnCreditsEnd.Invoke();
+		}
+	}
 }
