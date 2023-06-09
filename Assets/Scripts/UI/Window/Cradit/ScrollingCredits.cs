b1de8a7651e049856f21eafc4f51a59fafc51792
dayoff53 <42767781+dayoff53@users.noreply.github.com>
2023-06-10 07:10:57 +0900

    feat : 크래딧 End? 텍스트 피드백 수용 반영

--- Assets/Scripts/UI/Window/Cradit/ScrollingCredits.cs
+++ Assets/Scripts/UI/Window/Cradit/ScrollingCredits.cs
@@ -11,13 +11,16 @@ public class ScrollingCredits : MonoBehaviour
 	public GameObject endObject;
 	public List<string> credits = new List<string>();
 	public Transform startPoint;
+	public Transform meddlePoint;
 	public Transform endPoint;
 	public UnityEvent OnCreditsEnd;
 	public float cutOffPoint = 600f;
 	public float minimumSpacing = 100f;
+	public float endObjectDelayTime = 2f; // EndObject가 멈춘 후 OnCreditsEnd를 호출하기까지의 대기 시간
 	private List<GameObject> textObjects = new List<GameObject>();
 	[SerializeField]
 	private bool allTextsProcessed = false;
+	private bool endObjectReachedMeddlePoint = false; // endObject가 중간 지점에 도달했는지 확인
 
 	void Start()
 	{
@@ -26,20 +29,10 @@ public class ScrollingCredits : MonoBehaviour
 		CreateCredits();
 	}
 
-	void CreateCredits()
-	{
-		Vector3 currentPos = startPoint.position;
-		foreach (string credit in credits)
-		{
-			GameObject textObject = Instantiate(textPrefab, currentPos, Quaternion.identity, startPoint);
-			textObject.GetComponent<TextMeshProUGUI>().text = credit;
-			textObjects.Add(textObject);
-			currentPos -= new Vector3(0, minimumSpacing, 0);
-		}
-	}
-
 	void Update()
 	{
+		GameObject lastText = null;
+
 		for (int i = 0; i < textObjects.Count; i++)
 		{
 			GameObject currentText = textObjects[i];
@@ -58,13 +51,52 @@ public class ScrollingCredits : MonoBehaviour
 				textObjects.RemoveAt(i);
 				i--;
 			}
+			else
+			{
+				lastText = currentText;
+			}
+		}
+
+		if (lastText != null)
+		{
+			if (endObject.transform.localPosition.y < meddlePoint.localPosition.y * 2 && !endObjectReachedMeddlePoint)
+			{
+				endObject.transform.position = new Vector3(lastText.transform.position.x, lastText.transform.position.y - minimumSpacing, lastText.transform.position.z);
+			}
+			else if (!endObjectReachedMeddlePoint)
+			{
+				endObjectReachedMeddlePoint = true;
+				StartCoroutine(WaitAndInvokeEndCredits(endObjectDelayTime));
+			}
+
+			if (!endObject.activeSelf)
+			{
+				endObject.SetActive(true);
+			}
 		}
 		if (textObjects.Count == 0 && !allTextsProcessed)
 		{
-			FDebug.Log("CraditEnd");
+			Debug.Log("CreditEnd");
 			allTextsProcessed = true;
-			endObject.SetActive(true);
-			OnCreditsEnd.Invoke();
+		}
+	}
+
+	IEnumerator WaitAndInvokeEndCredits(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		OnCreditsEnd.Invoke();
+		Destroy(endObject);
+	}
+
+	void CreateCredits()
+	{
+		Vector3 currentPos = startPoint.position;
+		foreach (string credit in credits)
+		{
+			GameObject textObject = Instantiate(textPrefab, currentPos, Quaternion.identity, startPoint);
+			textObject.GetComponent<TextMeshProUGUI>().text = credit;
+			textObjects.Add(textObject);
+			currentPos -= new Vector3(0, minimumSpacing, 0);
 		}
 	}
 }
