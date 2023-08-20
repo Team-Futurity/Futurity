8a4a2cbba210ac3f4cde36a0f6fc7db47badabee
Budy123 <kth5948@naver.com>
2023-08-20 20:09:14 +0900

    Add : 유틸성 클래스, DebugLogger 추가

---
+++ Assets/Scripts/GameDebug/DebugLogger.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public static class DebugLogger
+{
+	public static void PrintDebugErros(List<string> msgs, Type componentType = null, DebugTypeEnum debugType = DebugTypeEnum.Log)
+	{
+		string name = componentType == null ? "System" : componentType.Name;
+
+		foreach (string msg in msgs)
+		{
+			switch(debugType)
+			{
+				case DebugTypeEnum.Log:
+					FDebug.Log($"[{name}] {msg}");
+					break;
+				case DebugTypeEnum.Warning:
+					FDebug.LogWarning($"[{name}] {msg}");
+					break;
+				case DebugTypeEnum.Error:
+					FDebug.LogError($"[{name}] {msg}");
+					break;
+
+			}
+		}
+	}
+}
