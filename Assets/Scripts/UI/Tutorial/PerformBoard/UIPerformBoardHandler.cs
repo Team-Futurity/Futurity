7b54ea99c221c56ed266105a6acf8b338982f7cd
rbase0218 <rbase@naver.com>
2023-09-03 18:33:26 +0900

    Fix : 코드 리팩터링 및 명시적 구현을 위한 인터페이스 메서드 추가

--- Assets/Scripts/UI/Tutorial/PerformBoard/UIPerformBoardHandler.cs
+++ Assets/Scripts/UI/Tutorial/PerformBoard/UIPerformBoardHandler.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIPerformBoardHandler : MonoBehaviour
+public class UIPerformBoardHandler : MonoBehaviour, IControllerMethod
 {
 	// Data를 전달받는 타겟 오브젝트
 	[field: Header("플레이어 데이터")]
@@ -21,39 +21,54 @@ public class UIPerformBoardHandler : MonoBehaviour
 	// Index
 	private int currentIndex;
 
+	private bool isActive;
+
 	private void Awake()
 	{
-		// if(PerformBoardList is null)
-		// {
-		// 	FDebug.LogError($"{PerformBoardList.GetType()}이 존재하지 않습니다.");
-		// 	
-		// 	Debug.Break();
-		// }
-		//
-		// currentIndex = 0;
-		// currentBoard = PerformBoardList[currentIndex];
+		if(PerformBoardList is null)
+		{
+			FDebug.LogError($"{PerformBoardList.GetType()}이 존재하지 않습니다.");
+			Debug.Break();
+		}
+
+		isActive = false;
+		currentIndex = 0;
+		currentBoard = PerformBoardList[currentIndex];
 	}
 
-	public void UpdateAction(PlayerInputEnum data)
+	#region IControllerMethod
+	void IControllerMethod.Active()
 	{
+		isActive = true;
+		
+		Target?.onChangeStateEvent.AddListener(UpdateAction);
 	}
 
-	public void CheckAllPass()
+	void IControllerMethod.Run()
 	{
-		OnNextBoard();
+		Target?.onChangeStateEvent.AddListener(UpdateAction);
 	}
 
-	private void OnNextBoard()
+	void IControllerMethod.Stop()
 	{
-		SetBoard();
+		Target?.onChangeStateEvent.RemoveListener(UpdateAction);
 	}
 
-	private void OnBeforeBoard()
+	#endregion
+	
+	private void UpdateAction(PlayerInputEnum data)
 	{
-		SetBoard();
+		if (!isActive)
+		{
+			return;
+		}
+		
+		var isComplate = currentBoard.SetPerformAction(data);
 	}
 
-	private void SetBoard()
+	private void SetNextBoard()
 	{
+		
 	}
+	
 }
