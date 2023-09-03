39bb1a0e71197464460a58f1b1ca545faba5ee34
rbase0218 <rbase@naver.com>
2023-09-04 00:31:05 +0900

    Feat : Dialog Type 정의

---
+++ Assets/Scripts/UI/Dialog/UIDialogType.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UIDialogType
+{
+	NONE = 0,
+	
+	NORMAL,
+	STANDING,
+	RANGE,
+	CUTSCENE,
+	
+	MAX
+}
