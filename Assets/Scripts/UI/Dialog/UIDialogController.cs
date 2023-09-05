832866c53c6ef47a0eab4e917b86a0715a601d4a
rbase0218 <rbase@naver.com>
2023-09-05 11:32:50 +0900

    Fix : DialogController Layer 게층 구조로 변경

--- Assets/Scripts/UI/Dialog/UIDialogController.cs
+++ Assets/Scripts/UI/Dialog/UIDialogController.cs
@@ -6,7 +6,8 @@ using UnityEngine.Events;
 
 public struct DialogData
 {
-	public string name;
+	public string talker_Eng;
+	public string talker_Kor;
 	public string descripton;
 }
 
@@ -18,26 +19,45 @@ public partial class UIDialogController : MonoBehaviour, IControllerMethod
 	
 	[field: Space(10)]
 	
+	[field: Header("텍스트가 출력되는 오브젝트")]
 	[field: SerializeField] 
 	public UIDialogText DialogText { get; private set; }
 
-	private bool isActive;
 
+	// Dialog System이 작동 되었을 때, 발동하는 이벤트
 	public UnityEvent onStart;
+	// Dialog System이 종료 되었을 때, 발동하는 이벤트
 	public UnityEvent onEnd;
+	// Dialog System이 변경 되었을 때, 발동하는 이벤트
 	public UnityEvent onChangeDialog;
 	
-	public UnityEvent<DialogData> onShow;
-	private DialogData testDialog;
+	// 내부적으로 사용되는 변경 이벤트
+	private UnityEvent<DialogData> onShow;
+
+	private List<DialogData> dialogDataList;
+
+	private DialogData currentDialog;
+	private DialogData nextDialog;
+
+	private int currentIndex = 0;
+
+	[SerializeField]
+	private GameObject imageObject;
+
+	// 현재 Dialog System이 살아있는가?
+	private bool isActive;
 
 	private void Awake()
 	{
 		isActive = false;
-
-		testDialog.name = "테스트";
-		testDialog.descripton = "임시로 텍스트를 작성하고 있습니다...";
 		
-		DataSetUp(DialogType);
+		if(imageObject == null)
+		{
+			FDebug.Log($"{imageObject.GetType()}이 존재하지 않습니다.");
+			FDebug.Break();
+		}
+
+		DataSetUp();
 	}
 	
 	// 테스트용 코드
@@ -68,35 +88,92 @@ public partial class UIDialogController : MonoBehaviour, IControllerMethod
 	
 	void IControllerMethod.Active()
 	{
-		
+		currentIndex = 0;
+		dialogDataList.Clear();
+
+		((IControllerMethod)this).Run();
 	}
 
 	void IControllerMethod.Run()
 	{
-		
+		isActive = true;
 	}
 
 	void IControllerMethod.Stop()
 	{
-		
+		isActive = false;
 	}
 	
 	#endregion
 
+	public void SetDialogData(string code)
+	{
+		if(isActive)
+		{
+			FDebug.LogError($"[Dialog System] 실행 중에는 Dialog 데이터를 변경할 수 없습니다.");
+			
+			return;
+		}
+
+		// Initialize
+		currentIndex = 0;
+		dialogDataList.Clear();
+
+		// Addressable Resource Path : Get Code
+		// dialogDataList = new List<DialogData>();
+
+		// 다음 출력 대화 세팅
+		nextDialog = dialogDataList[currentIndex++];
+		
+		// 현재 대화 세팅
+		SetCurrentData();
+	}
+
+	public void StartDialog()
+	{
+		if(isActive)
+		{
+			FDebug.LogError($"[Dialog System] 이미 시스템이 실행중입니다.");
+
+			return;
+		}
+
+		isActive = true;
+
+		imageObject.SetActive(true);
+	}
+
 	public void Show()
 	{
-		onShow?.Invoke(testDialog);
-		
-		DialogText.Show(testDialog.descripton);
+		// 한 대화를 보여주는 곳이므로, 데이터 세팅이 이 곳에서 이루어지면 된다.
+
+		//onShow?.Invoke(testDialog);
+		//DialogText.Show(testDialog.descripton);
 	}
-	
+
 	public void Skip()
 	{
-		
+		// Skipt은 해당 Dialog를 넘어간다.
+
 	}
 
 	public void Pass()
 	{
+		// Pass는 텍스트를 전부 추출한다.
+
 		DialogText.Pass();
+	}
+
+	private void SetCurrentData()
+	{
+		currentDialog = nextDialog;
+		nextDialog = dialogDataList[++currentIndex];
+	}
+
+	private void End()
+	{
+		// Dialog System이 종료된다.
+
+		onEnd?.Invoke();
 	}
 }
