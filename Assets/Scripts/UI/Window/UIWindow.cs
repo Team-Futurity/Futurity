c457be1f4ccce650f52f758d0860a19b4336e879
rbase0218 <rbase@naver.com>
2023-09-06 16:50:37 +0900

    Feat : Window의 기본 틀의 기능을 가지고 있는 UI Window 구현

--- Assets/Scripts/UI/Window/UIWindow.cs
+++ Assets/Scripts/UI/Window/UIWindow.cs
@@ -4,4 +4,63 @@ using UnityEngine;
 
 public class UIWindow : MonoBehaviour
 {
+	[field: SerializeField]
+	public WindowState CurrentState { get; private set; }
+	
+	[field: SerializeField]
+	public string WindowName { get; private set; }
+
+	public void Awake()
+	{
+		// WindowManager에 Window 등록하기
+		// WindowManager.Instance.AddWindow(WindowName, this);
+	}
+
+	// Window Manager에는 등록이 되어 있으나, 보이지 않는 상태.
+	public void ShowWindow()
+	{
+		if(CurrentState == WindowState.ASSIGN)
+		{
+			ChangeState(WindowState.ACTIVE);
+		}
+	}
+
+	// Window의 상태를 Assign으로 변경한다.
+	public void CloseWindow()
+	{
+		if(CurrentState == WindowState.ACTIVE)
+		{
+			ChangeState(WindowState.ASSIGN);
+		}
+	}
+
+	public void RemoveWindow()
+	{
+		// Window Memory에서 지워버린 상태.
+		if(CurrentState == WindowState.ACTIVE || CurrentState == WindowState.ASSIGN)
+		{
+			ChangeState(WindowState.UNASSIGN);
+		}
+	}
+
+	private void ChangeState(WindowState state)
+	{
+		if(state == WindowState.NONE || state == WindowState.MAX)
+		{
+			FDebug.Log($"[Window] 사용하지 않는 State 입니다.");
+			FDebug.Break();
+
+			return;
+		}
+
+		if(CurrentState == state)
+		{
+			FDebug.Log($"[Window] 동일한 Window로 변경하고 있습니다.");
+			FDebug.Break();
+
+			return;
+		}
+
+		CurrentState = state;
+	}
 }
