1a111e6e4e1df546424b10cfc4a1ed6e124ed5d7
rbase0218 <rbase@naver.com>
2023-09-06 16:50:48 +0900

    Feat : Window의 제어를 위한 컨트롤 시스템

--- Assets/Scripts/UI/Window/WindowManager.cs
+++ Assets/Scripts/UI/Window/WindowManager.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class WindowManager : Singleton<WindowManager>
 {
-	// Active Window를 관리하는 Buffer
-
-	// Window의 Data를 Dictionary로 관리하면서, 강제 종료 및 Open을 처리한다. [Admin]
 	private Dictionary<string, UIWindow> activeWindowDic;
+
+	private List<string> activeWindowNameList;
 
 	private bool isAdmin = false;
 
@@ -16,25 +15,12 @@ public class WindowManager : Singleton<WindowManager>
 		base.Awake();
 
 		activeWindowDic = new Dictionary<string, UIWindow>();
-	}
-
-	// Dictionary
-	// Add, Remove, Find, Active
-
-	public void RemoveWindow(string name)
-	{
-		
-		activeWindowDic?.Remove(name);
+		activeWindowNameList = new List<string>();
 	}
 
 	public void AddWindow(string name, UIWindow window)
 	{
 		activeWindowDic?.Add(name, window);
-	}
-
-	public void SetAdmin(bool isOn)
-	{
-		isAdmin = isOn;
 	}
 
 	public bool GetWindowState(string name)
@@ -47,23 +33,7 @@ public class WindowManager : Singleton<WindowManager>
 		return true;
 	}
 
-	#region Admin Method
-	
-	public void CloseWindow(string name)
-	{
-		if(!isAdmin)
-		{
-			FDebug.Log($"[Window Manager] 해당 권한은 어드민 권한이 필요로 합니다.");
-			return;
-		}	
-		
-		// Window를 강제로 닫게한다.
-		// Window들의 상태 변화는 Window 각 개체에서 진행하도록 한다.
-	}
-
-	#endregion
-
-	private bool HasWindow(string name)
+	public bool HasWindow(string name)
 	{
 		bool isHas = activeWindowDic.ContainsKey(name);
 
@@ -73,5 +43,40 @@ public class WindowManager : Singleton<WindowManager>
 		}
 
 		return isHas;
+	}
+
+	public void CloseWindow(string name)
+	{
+		if(!HasWindow(name))
+		{
+			return;
+		}
+
+		activeWindowDic[name].gameObject.SetActive(false);
+	}
+
+	public void ShowWindow(string name)
+	{
+		if(!HasWindow(name))
+		{
+			return;
+		}
+
+		activeWindowDic[name].gameObject.SetActive(true);
+	}
+
+	public void RemoveWindow(string name)
+	{
+		if (!HasWindow(name))
+		{
+			return;
+		}
+
+		activeWindowDic.Remove(name);
+	}
+
+	public void SetAdmin(bool isOn)
+	{
+		isAdmin = isOn;
 	}
 }
