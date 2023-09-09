ac48ea446cb046d58781ccc6fce6f8f999621a4e
rbase0218 <rbase@naver.com>
2023-09-09 17:46:37 +0900

    Add : Stage Controller 스크립트 추가

---
+++ Assets/Scripts/System/MapManager/StageController.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StageController : MonoBehaviour
+{
+
+}
