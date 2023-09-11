8c7597664bb76f2fd84677567c4a2494509e46d3
rbase0218 <rbase@naver.com>
2023-09-11 23:50:06 +0900

    Chore : 작업 환경 커밋

--- Assets/Scripts/System/MapManager/StageController.cs
+++ Assets/Scripts/System/MapManager/StageController.cs
@@ -7,5 +7,12 @@ using UnityEngine.Events;
 
 public class StageController : MonoBehaviour
 {
-
+	public AreaController currentArea;
+	
+	// Current Area
+	// Next Area
+	public void PlayGame()
+	{
+		
+	}
 }
