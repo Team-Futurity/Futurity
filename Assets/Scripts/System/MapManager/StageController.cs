e972c0d61c82fbf1f12d993e43ee4da898fcd723
rbase0218 <rbase@naver.com>
2023-09-12 18:54:26 +0900

    Fix : Stage Controller 필요 기능 주석 처리

--- Assets/Scripts/System/MapManager/StageController.cs
+++ Assets/Scripts/System/MapManager/StageController.cs
@@ -7,12 +7,44 @@ using UnityEngine.Events;
 
 public class StageController : MonoBehaviour
 {
-	public AreaController currentArea;
-	
-	// Current Area
-	// Next Area
-	public void PlayGame()
+	// 모든 Area 데이터 가지고 있기
+	[field: SerializeField]
+	public List<AreaController> areaControllerList;
+
+	// 현재 Area의 Index
+	[field: SerializeField]
+	public int currentAreaIndex = 0;
+	private int maxAreaIndex = 0;
+
+	// 현재 Area의 Init, Play, Stop 메서드 가지고 있기
+	// Init : 데이터 초기화
+	// Play : 즉각적인 게임 Play
+	// Stop : 게임 일시 중지 -> UI 제외
+
+	// 현재 Area가 Start Directing을 가지고 있을 경우 실행한다. -> Play에서 처리해줄 것
+	// 다음 씬으로 이동할 때, SetNextArea -> 네이밍 생각해볼 것.
+
+	private void Awake()
 	{
-		
+		maxAreaIndex = areaControllerList.Count;
+	}
+
+	public void SetNextArea()
+	{
+		// 인덱스 증가
+	}
+
+	public void InitArea()
+	{
+	}
+
+	public void PlayArea()
+	{
+
+	}
+
+	public void StopArea()
+	{
+
 	}
 }
