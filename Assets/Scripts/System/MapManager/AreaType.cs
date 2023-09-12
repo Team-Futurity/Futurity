29833b0fee2b34ba221653038fa114faac3183c4
rbase0218 <rbase@naver.com>
2023-09-12 19:50:11 +0900

    FIx : AreaType 수정

--- Assets/Scripts/System/MapManager/AreaType.cs
+++ Assets/Scripts/System/MapManager/AreaType.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// INIT_AREA : Stage에서 처음으로 실행되는 Area
-// LAST_AREA : Stage의 마지막 Area
-// NORMAL_AREA : 위 두개의 Type이 아닌 Area
-
 public enum AreaType
 {
 	NONE = 0,
 
-	INIT_AREA,
-	NORMAL_AREA,
-	LAST_AREA,
+	INIT,
+	LAST,
 
 	MAX
 }
