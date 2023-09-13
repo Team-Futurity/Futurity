1592725c2c39b58371537c258b49f0555ab555a7
j1nx09 <112684301+j1nx09@users.noreply.github.com>
2023-09-13 17:19:53 +0900

    Fix : animation issue

--- Assets/Scripts/Objects/Enemy/EnemyState/EliteDefault/EliteMeleeAttackState.cs
+++ Assets/Scripts/Objects/Enemy/EnemyState/EliteDefault/EliteMeleeAttackState.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EliteMeleeAttackState : MonoBehaviour
+public class EliteMeleeAttackState : EnemyAttackBaseState
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public override void Begin(EnemyController unit)
+	{
+		//FDebug.Log("MDefault Attack begin");
+		base.Begin(unit);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		//unit.atkCollider.enabled = true;
+		unit.navMesh.enabled = true;
+	}
+
+	public override void Update(EnemyController unit)
+	{
+		curTime += Time.deltaTime;
+		unit.DelayChangeState(curTime, unit.attackChangeDelay, unit, EnemyController.EnemyState.MDefaultAttack2nd);
+	}
+
+	public override void End(EnemyController unit)
+	{
+		//FDebug.Log("MDefault Attack End");
+
+		base.End(unit);
+		unit.atkCollider.enabled = false;
+	}
+
+	public override void OnTriggerEnter(EnemyController unit, Collider other)
+	{
+		//FDebug.Log("MDefault Attack Trigger");
+		base.OnTriggerEnter(unit, other);
+	}
 }
