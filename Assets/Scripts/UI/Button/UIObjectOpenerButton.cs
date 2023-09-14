6d97e6ec6beb5d3bf400df40cde5c1071306117a
rbase0218 <rbase@naver.com>
2023-09-14 14:11:38 +0900

    Fix : 기능 별 구성 요소 추가

--- Assets/Scripts/UI/Button/UIObjectOpenerButton.cs
+++ Assets/Scripts/UI/Button/UIObjectOpenerButton.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class UIObjectOpenerButton : UIButton
 {
+	[field: SerializeField]
+	public GameObject OpenPrefab { get; private set; }
+
 	protected override void SelectAction()
 	{
 		Debug.Log("SELECT" + GetType());
--- Assets/Scripts/UI/Button/UISceneLoadButton.cs
+++ Assets/Scripts/UI/Button/UISceneLoadButton.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class UISceneLoadButton : UIButton
 {
+	public float fadeTime = .0f;
+
 	protected override void SelectAction()
 	{
-		Debug.Log("SELECT" + GetType());
+		SceneLoader.Instance.LoadScene("LoadingScene 1");
 
+		//FadeManager.Instance.FadeIn(fadeTime, () =>
+		//{
+		//});
 	}
 }
