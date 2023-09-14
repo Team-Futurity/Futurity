c4ab30943ae883a4ea9531a45df524dc9e51293c
rbase0218 <rbase@naver.com>
2023-09-14 09:44:16 +0900

    Feat : SceneLoader - 씬 동기 호출 메서드 구현 완료

--- Assets/Scripts/System/SceneLoader/SceneLoader.cs
+++ Assets/Scripts/System/SceneLoader/SceneLoader.cs
@@ -1,7 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class SceneLoader : Singleton<SceneLoader>
 {
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public void LoadScene(string sceneName, LoadSceneMode mode = LoadSceneMode.Single, UnityAction endAction = null)
+	{
+		SceneManager.LoadScene(sceneName, mode);
+
+		endAction?.Invoke();
+	}
+
+	public void LoadSceneAsync(string sceneName, LoadSceneMode mode = LoadSceneMode.Single, UnityAction endAction = null)
+	{
+		var oper = SceneManager.LoadSceneAsync(sceneName, mode);
+
+		endAction?.Invoke();
+	}
 }
