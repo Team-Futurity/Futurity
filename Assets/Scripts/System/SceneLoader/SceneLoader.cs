8b8589434f844e5b341bb2afd06f95b18badb1bc
rbase0218 <rbase@naver.com>
2023-09-15 17:33:07 +0900

    Fix : Loading Scene을 SceneLoader에서 내부적 캐싱으로 변경

--- Assets/Scripts/System/SceneLoader/SceneLoader.cs
+++ Assets/Scripts/System/SceneLoader/SceneLoader.cs
@@ -8,14 +8,39 @@ public class SceneLoader : Singleton<SceneLoader>
 {
 	public float sceneProgress = .0f;
 
+
+	// Only Title
+	private readonly string LoadingScene1 = "LoadingScene 1";
+
+	private readonly string LoadingScene2 = "LoadingScene 2";
+
+	private string nextSceneName = "";
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		SceneManager.sceneLoaded += Test;
+	}
+
+	private void Test(Scene scene, LoadSceneMode mode)
+	{
+		GameObject.Find("Loading System Group").TryGetComponent<LoadingSystem>(out var loadSystem);
+		loadSystem.SetNextScene(nextSceneName);
+	}
+
 	public void LoadScene(string sceneName)
 	{
-		SceneManager.LoadScene(sceneName);
+		nextSceneName = sceneName;
+
+		SceneManager.LoadScene((sceneName == "TutorialScene") ? LoadingScene1 : LoadingScene2);
 	}
 
 	public void LoadScene(string sceneName, LoadSceneMode mode = LoadSceneMode.Single, UnityAction endAction = null)
 	{
-		SceneManager.LoadScene(sceneName, mode);
+		nextSceneName = sceneName;
+
+		SceneManager.LoadScene((sceneName == "TutorialScene") ? LoadingScene1 : LoadingScene2, mode);
 
 		endAction?.Invoke();
 	}
@@ -43,6 +68,7 @@ public class SceneLoader : Singleton<SceneLoader>
 
 			yield return null;
 
+			// SceneProgress가 95% 이상 Load되고, Time이 2초 이상 지났을 경우 Scene Load
 			if (sceneProgress > 0.95f && timer >= 2f)
 			{
 				FadeManager.Instance.FadeIn(1f, () =>
