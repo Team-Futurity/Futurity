30397d85fcac76ba6a3cfbe071e5ccfc8524bc1e
rbase0218 <rbase@naver.com>
2023-09-16 20:11:32 +0900

    Fix : Tutorial Manager 개발 중

--- Assets/Scripts/System/Tutorial/TutorialManager.cs
+++ Assets/Scripts/System/Tutorial/TutorialManager.cs
@@ -1,8 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
+	[SerializeField] private float fadeTime = 1f;
 
+	[SerializeField] private UIPerformBoardHandler performHanlder;
+
+	[SerializeField] private UIDialogController dialogController;
+
+	private IControlCommand dialogCommand;
+	private IControlCommand perfromCommand;
+	
+	private void Awake()
+	{
+		dialogController.TryGetComponent(out dialogCommand);
+		performHanlder.TryGetComponent(out dialogCommand);
+	}
+
+	private void Start()
+	{
+		FadeManager.Instance.FadeOut(fadeTime, () =>
+		{
+			StartTutorial();
+		});
+	}
+
+	private void StartTutorial()
+	{
+		dialogController.SetDialogData("TEST");
+		dialogController.ShowDialog();
+		dialogController.PlayDialog();
+		
+		// Dialog End에 Perform Action 넣어주고
+		// Dialog의 모든 End를 Clear로 판단하자.
+	}
+	
 }
