b82d4dd1a59b4952d0dcf4a9fc51fe366b1700ed
Budy123 <kth5948@naver.com>
2023-09-17 18:50:29 +0900

    Fix : 레거시 코드 삭제

--- Assets/Scripts/Objects/Player/PlayerAttack/Editor/ComboGaugeCustomEditor.cs
+++ Assets/Scripts/Objects/Player/PlayerAttack/Editor/ComboGaugeCustomEditor.cs
@@ -11,10 +11,6 @@ public class ComboGaugeCustomEditor : Editor
 
 		var comboGaugeSystem = (ComboGaugeSystem)target;
 
-		// 변수 변경
-		// comboGaugeSystem.gaugeBar = comboGaugeSystem.gaugeBar == null ? GameObject.Find("HitGauge")?.GetComponent<GaugeBarController>() : comboGaugeSystem.gaugeBar;
-		//if(comboGaugeSystem.gaugeBar == null ) { FDebug.LogWarning("[ComboGaugeCustomEditor] ComboGaugeBar를 찾을 수 없음. Plyer 오브젝트에 GaugeBar에 할당 필요."); }
-
 		// ComboData 리스트 변경
 		comboGaugeSystem.comboData = comboGaugeSystem.comboData == null ? new List<ComboCountData>() : comboGaugeSystem.comboData;
 		var list = comboGaugeSystem.comboData;
