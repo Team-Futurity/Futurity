7990483169c6b098ea9f2fc7411f6c1b5de3b8bf
Budy123 <kth5948@naver.com>
2023-09-17 18:13:23 +0900

    Fix : 과거의 잔재 삭제

--- Assets/Scripts/Objects/Enemy/EnemyState/Base/EnemySpawnState.cs
+++ Assets/Scripts/Objects/Enemy/EnemyState/Base/EnemySpawnState.cs
@@ -26,9 +26,6 @@ public class EnemySpawnState : UnitState<EnemyController>
 
 	public override void Update(EnemyController unit)
 	{
-		if (true)
-			return;
-		
 		curTime += Time.deltaTime;
 
 		if (refColor.a > 0f)
