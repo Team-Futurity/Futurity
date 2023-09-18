266db0068a4b8f39e38afb180211283d3970e565
Hyoung-Seok <dkavmdk98@gmail.com>
2023-09-19 04:10:53 +0900

    Feat : 스테이지 전환 임시 구현

---
+++ Assets/Scripts/Test/MoveTrigger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrigger : MonoBehaviour
+{
+	[SerializeField] private Transform player;
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			FadeManager.Instance.FadeIn();
+			
+			Invoke("MovePlayer", 1.3f);
+		}
+	}
+
+	private void MovePlayer()
+	{
+		SteageMove.Instance.MoveStage(SteageMove.EStageType.STAGE_3);
+		FadeManager.Instance.FadeOut();
+	}
+}
