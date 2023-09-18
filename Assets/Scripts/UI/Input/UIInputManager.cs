35df71863c4bc0104ea821e03f903d6b5a23874e
rbase0218 <rbase@naver.com>
2023-09-18 19:57:19 +0900

    Fix : PlayerInput의 Assets을 InputActionManager에서 받아 사용

--- Assets/Scripts/UI/Input/UIInputManager.cs
+++ Assets/Scripts/UI/Input/UIInputManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UIInputManager : Singleton<UIInputManager>
 {
 	private Dictionary<int, UIButton> buttonDic = new Dictionary<int, UIButton>();
+	private PlayerInput playerInput;
 
 	private int currentIndex = 0;
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		TryGetComponent(out playerInput);
+	}
+
+	private void Start()
+	{
+		if (InputActionManager.Instance.IsActive(InputActionType.UI))
+		{
+			SetInputAction(InputActionManager.Instance.GetByType(InputActionType.UI));
+		}
+	}
+
+	public void SetInputAction(InputActionAsset asset)
+	{
+		playerInput.actions = asset;
+	}
 
 	public void AddButton(int order, UIButton button)
 	{
 		buttonDic?.Add(order, button);
-
-		SelectUI();
 	}
 
 	public void ClearAll()
@@ -20,16 +40,16 @@ public class UIInputManager : Singleton<UIInputManager>
 		buttonDic.Clear();
 	}
 
-	private void SelectUI()
+	public void SelectUI()
 	{
 		buttonDic[currentIndex].Select();
 	}
-	
+
 	private void ChangeToIndex(int num)
 	{
 		var result = currentIndex + num;
 
-		if(result < 0 || result >= buttonDic.Count)
+		if (result < 0 || result >= buttonDic.Count)
 		{
 			return;
 		}
@@ -55,6 +75,11 @@ public class UIInputManager : Singleton<UIInputManager>
 
 	public void OnClickUI()
 	{
+		if (buttonDic == null)
+		{
+			return;
+		}
+
 		buttonDic[currentIndex].Active();
 	}
 	#endregion
