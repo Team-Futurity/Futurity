38c2d91b204074d0553c7095520b1259f1da6d48
Hyoung-Seok <dkavmdk98@gmail.com>
2023-09-21 16:44:13 +0900

    Fix : 피격 HitStop 플레이어 애니메이션 이벤트에서 실행하도록 변경

--- Assets/00Scripts/01Objects/Player/PlayerAnimationEvents.cs
+++ Assets/00Scripts/01Objects/Player/PlayerAnimationEvents.cs
@@ -13,6 +13,7 @@ public class PlayerAnimationEvents : MonoBehaviour
 	private EffectActivationTime effectType;
 	private EffectTarget EffectTarget;
 	private Transform effectPos;
+	private IEnumerator hitStop;
 
 	public FMODUnity.EventReference walk;
 
@@ -155,5 +156,18 @@ public class PlayerAnimationEvents : MonoBehaviour
 		bool isActive = isActiveInteager == 1;
 
 		pc.SetCollider(isActive);
+	}
+	
+	public void StartHitStop(float duration)
+	{
+		hitStop = HitStop(duration);
+		StartCoroutine(hitStop);
+	}
+	
+	private IEnumerator HitStop(float duration)
+	{
+		Time.timeScale = 0.0f;
+		yield return new WaitForSecondsRealtime(duration);
+		Time.timeScale = 1.0f;
 	}
 }
