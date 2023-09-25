2d8b7301d9795c69cd6aea94b492cc0a4f56469c
rbase0218 <rbase@naver.com>
2023-09-25 17:53:12 +0900

    Fix : 테스트 환경 설정 및 구조 배치

--- Assets/00Scripts/00System/Part/PartBehaviour.cs
+++ Assets/00Scripts/00System/Part/PartBehaviour.cs
@@ -7,4 +7,7 @@ public class PartBehaviour : MonoBehaviour
 	// Activation Percent
 	// Part Code -> 
 	// Part Type -> Active, Passive
+	
+	// Sub의 경우, 활성 구간이 25 혹은 50의 경우 바로 PartSystem으로 넘길 수 있도록 한다.
+	
 }
--- Assets/00Scripts/00System/Part/PartSystem.cs
+++ Assets/00Scripts/00System/Part/PartSystem.cs
@@ -1,11 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PartSystem : MonoBehaviour
 {
-	// Player Equip Part
-	// Active - 1
-	// Passive - 3
+	[SerializeField, Header("Combo 시스템")]
+	private ComboGaugeSystem comboGaugeSystem;
 	
+	[SerializeField, Header("장착 부품 목록")]
+	private List<PartBehaviour> equipPartList = new List<PartBehaviour>();
+	
+	[SerializeField, Header("디버그 용")]
+	private List<StatusData> status;
+
+	private void Awake()
+	{
+		status.Clear();
+	}
+
+	public void EquipPart()
+	{
+		
+	}
+
+	public void UnEquipPart()
+	{
+		
+	}
+
+	// Combo System에 부품 추가
+	private void AddComboEventToPart()
+	{
+		
+	}
+
+	// Combo System에서 부품 제거
+	private void RemoveComboEventToPart()
+	{
+		
+	}
 }
