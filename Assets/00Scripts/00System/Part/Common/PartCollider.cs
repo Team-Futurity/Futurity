85dad29d4fe4969681a15586a10316d5011a292a
rbase0218 <rbase@naver.com>
2023-09-25 17:22:31 +0900

    Fix : Part 구성 요소 스크립트 추가

---
+++ Assets/00Scripts/00System/Part/Common/PartCollider.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PartCollider
+{
+	public static void DrawCircleCollider(Vector3 createPos, float radius)
+	{
+		
+	}
+
+	public static void DrawRectCollider(Vector3 startPos, Vector3 endPos)
+	{
+		
+	}
+}
---
+++ Assets/00Scripts/00System/Part/PartSystem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartSystem : MonoBehaviour
+{
+	// Player Equip Part
+	// Active - 1
+	// Passive - 3
+	
+}
