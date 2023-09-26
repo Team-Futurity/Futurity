dd8ead95cfc038c2083bd11812469724187a295e
rbase0218 <rbase@naver.com>
2023-09-26 16:03:32 +0900

    Refactor : Core Ability 구현 필요 내용 작성

--- Assets/00Scripts/00System/Part/Base/CoreAbility.cs
+++ Assets/00Scripts/00System/Part/Base/CoreAbility.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CoreAbility : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Core Type : Attack, Collider
+	// Attack Type : Status or Damage
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// 상태 이상 : Buff System 적극 활용
+	// 
+
+	// Attack
+	// 직접 어택, PlayerController Update를 받아서
+
+	// Core 
+	// Player Controller
+	// - 몬스터 직접 어택 - 감마, 로우
+	// - 사망한 몬스터 공격 범위 생성 
+
+	// Collider
+	// - 콜라이더 생성 ( 반지름, 몬스터 피해량, 형태, 시간 ) -? 전이 가능 형태??
 }
