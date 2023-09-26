a919739dda3332af9a6c95e75238b4ed322cdc0f
rbase0218 <rbase@naver.com>
2023-09-26 14:43:28 +0900

    Fix : Part System에서 Active, UnActive 구분 로직 변경

--- Assets/00Scripts/00System/Part/PartSystem.cs
+++ Assets/00Scripts/00System/Part/PartSystem.cs
@@ -7,37 +7,91 @@ public class PartSystem : MonoBehaviour
 {
 	[SerializeField, Header("Combo 시스템")]
 	private ComboGaugeSystem comboGaugeSystem;
-	
+
+	// 0 ~ 2	: Passive
+	// 3		: Active
 	[SerializeField, Header("장착 부품 목록")]
 	private List<PartBehaviour> equipPartList = new List<PartBehaviour>();
-	
+
 	[SerializeField, Header("디버그 용")]
 	private List<StatusData> status;
 
+	public float debugPercent = .0f;
+
 	private void Awake()
 	{
-		status.Clear();
+		ClearStatus();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			UpdateComboGauge(debugPercent);
+		}
 	}
 
 	public void EquipPart()
 	{
-		
+
 	}
 
 	public void UnEquipPart()
 	{
-		
+
 	}
 
-	// Combo System에 부품 추가
-	private void AddComboEventToPart()
+	// Combo System에서 Update 추가하기.
+	private void UpdateComboGauge(float percent)
 	{
-		
+		int activePossibleCount = (int)Math.Floor(percent / 25f);
+		int maxPartCount = equipPartList.Count;
+
+		// 25, 50은 Sub만 실행
+		// 75는 Sub, Core 실행
+
+		// Active
+		for (int i = 1; i <= ((activePossibleCount > maxPartCount) ? maxPartCount : activePossibleCount); ++i)
+		{
+			AddStatus(equipPartList[i - 1].GetSubAbility());
+		}
+
+		// UnActive
+		for (int i = maxPartCount; i > activePossibleCount; --i)
+		{
+
+		}
 	}
 
-	// Combo System에서 부품 제거
-	private void RemoveComboEventToPart()
+	private void AddStatus(List<StatusData> statusData)
 	{
-		
+		foreach (var statusElement in statusData)
+		{
+			var hasStatus = (status.Find((x) => x.type == statusElement.type) is null);
+
+			if (hasStatus)
+			{
+				var element = status.Find((x) => x.type == statusElement.type);
+				element = statusElement;
+			}
+			else
+			{
+				Debug.Log("왜 여기로 들어가?");
+				status.Add(statusElement);
+			}
+		}
+	}
+
+	private void SubStatus(List<StatusData> statusData)
+	{
+		foreach (var statusElement in statusData)
+		{
+
+		}
+	}
+
+	private void ClearStatus()
+	{
+		status.Clear();
 	}
 }
