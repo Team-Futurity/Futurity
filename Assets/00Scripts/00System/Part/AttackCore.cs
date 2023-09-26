ff589fda16b5abc8ee432d86f87e8a8a58b314cb
rbase0218 <rbase@naver.com>
2023-09-26 22:31:07 +0900

    Fix : AttackCore 중, 범위 공격 구현

--- Assets/00Scripts/00System/Part/AttackCore.cs
+++ Assets/00Scripts/00System/Part/AttackCore.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AttackCore : CoreAbility
@@ -16,12 +17,16 @@ public class AttackCore : CoreAbility
 	// 2. 직접 데미지
 	[SerializeField]
 	private AttackCoreType attackType;
+
+	[SerializeField]
+	private LayerMask targetLayer;
 	
 	// 공격 시, 전이 효과
 	[SerializeField]
 	private bool isStateTransition;
 
-	private float attackDamage = .0f;
+	public float attackDamage = .0f;
+	public float attackColliderRadius = .0f;
 	
 	// 전이 효과가 존재할 경우, 충돌 Circle 사이즈
 	public float transitionColliderRadius = .0f;
@@ -29,10 +34,38 @@ public class AttackCore : CoreAbility
 	public int transitionCount = 0;
 	
 	// Monster Data
+	private List<int> hitEnemyList = new List<int>();
 	
-	protected override void OnPartAbility(EnemyController enemy)
+	protected override void OnPartAbility(Enemy enemy)
 	{
 		// Attack 시, Buff 적용
+		enemy.TryGetComponent<UnitBase>(out var enemyUnit);
+
+		enemyUnit.Hit(null, attackDamage);
+		hitEnemyList.Add(enemyUnit.GetInstanceID());
+
+		var coll = PartCollider.DrawCircleCollider(transform.position, attackColliderRadius, targetLayer);
+		
+		AttackEnemyInCollider(coll);
 	}
-	
+
+	private void AttackEnemyInCollider(Collider[] enemyCollider)
+	{
+		foreach (var enemy in enemyCollider)
+		{
+			enemy.TryGetComponent<UnitBase>(out var enemyUnit);
+			
+			var alreadyHit = hitEnemyList.Contains(enemyUnit.GetInstanceID());
+			
+			if (alreadyHit)
+			{
+				continue;
+			}
+			
+			enemyUnit.Hit(null, attackDamage);
+			hitEnemyList.Add(enemyUnit.GetInstanceID());
+		}
+		
+		hitEnemyList.Clear();
+	}
 }
