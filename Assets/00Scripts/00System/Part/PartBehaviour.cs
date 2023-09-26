e9f7efd0d709ebd801bbde15eb84d314b50d6d76
rbase0218 <rbase@naver.com>
2023-09-26 16:03:02 +0900

    FIx : Part Behaviour 초기 로직 구현

--- Assets/00Scripts/00System/Part/PartBehaviour.cs
+++ Assets/00Scripts/00System/Part/PartBehaviour.cs
@@ -4,10 +4,57 @@ using UnityEngine;
 
 public class PartBehaviour : MonoBehaviour
 {
+	// Gauge를 제어하는 것은 Part System에서 진행한다. -> Part 넣는 위치에 따라서 달라지기 때문
+
 	// Activation Percent
-	// Part Code -> 
-	// Part Type -> Active, Passive
+	public int partCode = 0;
+	public PartType partType = PartType.NONE;
 	
-	// Sub의 경우, 활성 구간이 25 혹은 50의 경우 바로 PartSystem으로 넘길 수 있도록 한다.
-	
+	// Player의 개념으로 Sub와 Core를 사용할 수 있도록 한다.
+	private SubAbility subAbility;
+	private CoreAbility coreAbility;
+
+	private bool isActive = false;
+
+	private void Awake()
+	{
+		TryGetComponent(out coreAbility);
+
+		if (partType == PartType.PASSIVE)
+		{
+			TryGetComponent(out subAbility);
+		}
+
+		isActive = false;
+	}
+
+	public void OnFeature()
+	{
+
+	}
+
+	public void SetPartActive(bool isOn)
+	{
+		isActive = isOn;
+	}
+
+	public bool GetPartActive()
+	{
+		return isActive;
+	}
+
+	public void GetCoreAbility()
+	{
+
+	}
+
+	public List<StatusData> GetSubAbility()
+	{
+		if (!isActive)
+		{
+			return null;
+		}
+
+		return subAbility.GetSubAbilityData();
+	}
 }
