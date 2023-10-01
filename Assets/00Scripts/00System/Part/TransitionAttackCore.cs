7455111699a7771db2ef97a35c4bc735d788f6f5
rbase0218 <rbase@naver.com>
2023-10-01 23:26:16 +0900

    Fix : 같은 전이 공격에만 중첩되지 않도록 변경

--- Assets/00Scripts/00System/Part/TransitionAttackCore.cs
+++ Assets/00Scripts/00System/Part/TransitionAttackCore.cs
@@ -2,74 +2,100 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEditor.U2D.Path;
 using UnityEngine;
 
 public class TransitionAttackCore : MonoBehaviour
 {
-	private float colliderRadius = .0f;
-	private int attackCount = 0;
-	private float attackDamage = .0f;
-	private LayerMask targetLayer;
+	private TransitionProtocol protocol;
 
-	private bool isSearchNextMonster = false;
-
-	public void SetTransitionData(float radius, int count, float damage, LayerMask target)
+	public void SetTransitionData(TransitionProtocol receive)
 	{
-		Debug.Log($"Enemy Name {transform.name}");
-		
-		colliderRadius = radius;
-		attackCount = count;
-		attackDamage = damage;
-		targetLayer = target;
+		protocol = receive;
 	}
 
-	public void PlayAction(float delay = .0f)
+	public void Play(float delay = .0f)
+	{
+		PlayAction(delay);
+	}
+
+	public bool CompareID(int id)
+	{
+		return (protocol.transitionID == id);
+	}
+
+	private void PlayAction(float delay = .0f)
 	{
 		AddDamage();
-		
-		var nextEnemy = ExploreNearEnemy();
 
-		if (attackCount <= 0 || nextEnemy == null)
+		if (protocol.transitionCount > 0)
 		{
-			return;
+			ExploreNearEnemy();
 		}
-		
-		var hasTransitionComponent = nextEnemy.TryGetComponent(out TransitionAttackCore transitionAttackCore);
-
-		if (!hasTransitionComponent)
-		{
-			transitionAttackCore = nextEnemy.gameObject.AddComponent<TransitionAttackCore>();
-		}
-			
-		transitionAttackCore.SetTransitionData(colliderRadius, attackCount - 1, attackDamage, targetLayer);
-		transitionAttackCore.PlayAction(1f);
 	}
 
-	private Collider ExploreNearEnemy()
+	private bool AddTransitionComponent(Collider enemy)
 	{
-		var coll = PartCollider.DrawCircleCollider(transform.position, colliderRadius, targetLayer);
+		// 다음 enemy
+		var hasTransitionComponent = enemy.TryGetComponent(out TransitionAttackCore transition);
+			
+		// 가지고 있지 않을 경우
+		if (!hasTransitionComponent)
+		{
+			transition = enemy.AddComponent<TransitionAttackCore>();
+		}
+
+		// 동일하다는 것은 다음 것을 불러와야 한다.
+		if (transition.CompareID(protocol.transitionID))
+		{
+			return false;
+		}
+		
+		protocol.transitionCount--;
+
+		transition.SetTransitionData(protocol);
+		transition.PlayAction();
+		
+		return true;
+	}
+
+	private void ExploreNearEnemy()
+	{
+		var coll = PartCollider.DrawCircleCollider(transform.position, protocol.transitionColliderRadius, protocol.targetLayer);
 		
 		if (coll.Length <= 1)
 		{
-			return null;
+			return;
 		}
-		
-		var nearEnemy = coll.OrderBy((x) => Vector3.Distance(x.transform.position, transform.position)).ToList()[1];
-		
-		return nearEnemy;
+
+		// 몬스터를 정렬한다.
+		var nearEnemyColliders = coll.OrderBy((x) => Vector3.Distance(x.transform.position, transform.position)).ToList();
+
+		// 순회한다.
+		foreach (var nearEnemy in nearEnemyColliders)
+		{
+			// nearEnemy를 넘겨서 조건에 부합한지 확인한다.
+			var isFindNearEnemy = AddTransitionComponent(nearEnemy);
+
+			if (isFindNearEnemy)
+			{
+				return;
+			}
+		}
 	}
 
 	private void AddDamage()
 	{
 		TryGetComponent(out Enemy enemyUnit);
-		
-		enemyUnit.Hit(null, attackDamage);
+		enemyUnit.Hit(null, protocol.attackDamage);
+		Debug.Log($"ATTACK + " + transform.name);
 	}
 
 	private void OnDrawGizmos()
 	{
 		Gizmos.color = Color.red;
-		Gizmos.DrawWireSphere(transform.position, colliderRadius);
+		Gizmos.DrawWireSphere(transform.position, protocol.transitionColliderRadius);
 	}
+	
 }
