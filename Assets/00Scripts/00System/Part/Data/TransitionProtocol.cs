b82629b77499d9f3cfc77654c1af4a5160a032af
rbase0218 <rbase@naver.com>
2023-10-01 23:17:42 +0900

    Fix : int -> float로 attackDamage 수정

--- Assets/00Scripts/00System/Part/Data/TransitionProtocol.cs
+++ Assets/00Scripts/00System/Part/Data/TransitionProtocol.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public struct TransitionProtocol
 {
 	public int transitionID;
 	public float transitionColliderRadius;
-	public int attackDamage;
+	public float attackDamage;
 	public int transitionCount;
 	public LayerMask targetLayer;
 
-	public TransitionProtocol(int id, float radius, int damage, int count, LayerMask layer)
+	public TransitionProtocol(int id, float radius, float damage, int count, LayerMask layer)
 	{
 		transitionID = id;
 		transitionColliderRadius = radius;
