466b28c3c0737e31cff5e63704672f5752badde0
rbase0218 <rbase@naver.com>
2023-10-04 04:34:18 +0900

    Fix : ColliderCore 스크립트 추가

--- Assets/00Scripts/00System/Part/ColliderCore.cs
+++ Assets/00Scripts/00System/Part/ColliderCore.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,41 @@ public class ColliderCore : CoreAbility
 	// 람다 : 시간 확인 후, 상태 이상 부여 -> Player
 	// 에타 : 사망한 모든 몬스터의 자리에 공격 범위 생성, 상태 이상 부여 -> Death Monster
 
+	public ColliderCoreType coreActiveType;
+
 	// 람다 & 에타
 	public float colliderRadius = .0f;
 	public float colliderExecuteCycle = .0f;
+
+	private float timer = .0f;
 	
 	protected override void OnPartAbility(Enemy enemy)
 	{
-		
+		switch (coreActiveType)
+		{
+			case ColliderCoreType.ON_SCHEDULE_TIME:
+				break;
+			
+			case ColliderCoreType.ENEMY_DEATH:
+				break;
+		}
+	}
+
+	private void Update()
+	{
+		if (coreActiveType != ColliderCoreType.ON_SCHEDULE_TIME)
+		{
+			return;
+		}
+
+		timer += Time.deltaTime;
+
+		if (timer >= colliderExecuteCycle)
+		{
+			timer = .0f;
+			
+			ExploreEnemy();
+		}
 	}
 
 	private void ExploreEnemy()
