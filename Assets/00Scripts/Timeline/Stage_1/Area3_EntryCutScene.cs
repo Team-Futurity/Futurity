21a1ebad6464874af6a587e6393a3a6fb7d461d9
Hyoung-Seok <dkavmdk98@gmail.com>
2023-10-06 18:06:44 +0900

    Style : 주석 코드 제거

--- Assets/00Scripts/Timeline/Stage_1/Area3_EntryCutScene.cs
+++ Assets/00Scripts/Timeline/Stage_1/Area3_EntryCutScene.cs
@@ -80,29 +80,7 @@ public class Area3_EntryCutScene : CutSceneBase
 	
 		curScriptsIndex = (curScriptsIndex + 1 < scriptsList.Count) ? curScriptsIndex + 1 : 0;
 	}
-
-	// private IEnumerator PrintScripts()
-	// {
-	// 	while (manager.scripting.isEnd == false)
-	// 	{
-	// 		yield return null;
-	// 	}
-	// 	
-	// 	chapter1Director.Resume();
-	// 	manager.scripting.isEnd = false;
-	//
-	// 	if (curScriptsIndex + 1 < scriptsList.Count)
-	// 	{
-	// 		curScriptsIndex++;
-	// 		yield return waitForSecondsRealtime;
-	// 		manager.scripting.InitNameField(scriptsList[curScriptsIndex].scriptList[0].name);
-	// 	}
-	// 	else
-	// 	{
-	// 		curScriptsIndex = 0;
-	// 	}
-	// }
-	//
+	
 	public void MovePlayer()
 	{
 		manager.PlayerController.LerpToWorldPosition(playerMoveTarget.position, 1.5f);
