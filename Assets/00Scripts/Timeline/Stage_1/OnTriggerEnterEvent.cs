ae77857873bae915f97460b9de47b08201766490
Hyoung-Seok <dkavmdk98@gmail.com>
2023-10-06 20:22:50 +0900

    Feat : 보상상자 임시 상호작용 스크립트 작성

--- Assets/00Scripts/Timeline/Stage_1/OnTriggerEnterEvent.cs
+++ Assets/00Scripts/Timeline/Stage_1/OnTriggerEnterEvent.cs
@@ -6,9 +6,21 @@ using UnityEngine.Events;
 
 public class OnTriggerEnterEvent : MonoBehaviour
 {
+	[SerializeField] private RewardBox rewardBox;
+	
 	private void OnTriggerEnter(Collider other)
 	{
-		if (other.CompareTag("Player"))
+		if (other.CompareTag("Player") == false && rewardBox.isCutScenePlayed == false)
+		{
+			return;
+		}
+
+		if (rewardBox.isCutScenePlayed == false)
+		{
+			TimelineManager.Instance.EnableCutScene(ECutScene.AREA1_REWARDCUTSCENE);
+			rewardBox.isCutScenePlayed = true;
+		}
+		else if (rewardBox.isInteraction == true)
 		{
 			TimelineManager.Instance.EnableCutScene(ECutScene.AREA1_EXITCUTSCENE);
 		}
