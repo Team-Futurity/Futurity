4bcad787e1fa300f6c0b39ec1c5536c9218b76c8
Hyoung-Seok <dkavmdk98@gmail.com>
2023-10-09 19:08:57 +0900

    Feat : 연출 재생 구현

--- Assets/00Scripts/Test/ForceFadeIn.cs
+++ Assets/00Scripts/Test/ForceFadeIn.cs
@@ -7,6 +7,12 @@ public class ForceFadeIn : MonoBehaviour
 {
 	private void Start()
 	{
-		FadeManager.Instance.FadeOut();
+		FadeManager.Instance.FadeOut(1.0f);
+		Invoke("PlayCutScene", 0.7f);
+	}
+
+	private void PlayCutScene()
+	{
+		TimelineManager.Instance.EnableCutScene(ECutScene.BOSS_ENTRYCUTSCENE);
 	}
 }
