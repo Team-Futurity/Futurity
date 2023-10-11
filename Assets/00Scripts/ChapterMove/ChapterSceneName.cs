a94f61cfb30da3ad98700483e11021e81703b929
Hyoung-Seok <dkavmdk98@gmail.com>
2023-10-11 15:24:26 +0900

    Fix : 각 챕터 enum값 귀속

--- Assets/00Scripts/ChapterMove/ChapterSceneName.cs
+++ Assets/00Scripts/ChapterMove/ChapterSceneName.cs
@@ -1,6 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+public enum EChapterType
+{
+	CHAPTER1_1,
+	CHAPTER1_2,
+	CHAPTER_BOSS
+}
 
 public static class ChapterSceneName
 {
