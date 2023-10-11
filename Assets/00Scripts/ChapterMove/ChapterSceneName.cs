1bf7158b122d0b2a5151a370a92b658a798d2548
Hyoung-Seok <dkavmdk98@gmail.com>
2023-10-11 14:50:51 +0900

    Feat : Chapter 씬 이름 저장 클래스 생성

---
+++ Assets/00Scripts/ChapterMove/ChapterSceneName.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ChapterSceneName
+{
+	public const string CHAPTER1_1 = "Chapter1-Stage1";
+	public const string CHAPTER1_2 = "Chapter1-Stage2";
+	public const string BOSS_CHAPTER = "BossChapter";
+}
