d4e7d45edbc78966d623ab1f80d3441a06b41722
rbase0218 <rbase@naver.com>
2023-10-11 18:30:22 +0900

    Fix : 더미 데이터 삭제 및 PartEquip Window 스크립트 추가

--- Assets/00Scripts/03UI/Part/UIPartEquip.cs
+++ Assets/00Scripts/03UI/Part/UIPartEquip.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class UIPartEquip : MonoBehaviour
 {
-	
+	// 0, 1, 2 -> Top, Middle, Bottom
+	public List<UIPartSelectButton> passiveButton;
+
+	public UIPassiveSelectData dummyData1;
+	public UIPassiveSelectData dummyData2;
+	public UIPassiveSelectData dummyData3;
+
+	public void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			passiveButton[0].ChangeResource(dummyData1);
+			passiveButton[1].ChangeResource(dummyData2);
+			passiveButton[2].ChangeResource(dummyData3);
+		}
+	}
+
+	public void SetPartData()
+	{
+
+	}
 }
