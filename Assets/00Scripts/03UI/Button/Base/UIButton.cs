9d5506ac5905284d64fba9cd9b89ed1aa43048d2
rbase0218 <rbase@naver.com>
2023-10-11 18:03:58 +0900

    Refactor : 불필요 코드 제거

--- Assets/00Scripts/03UI/Button/Base/UIButton.cs
+++ Assets/00Scripts/03UI/Button/Base/UIButton.cs
@@ -9,8 +9,6 @@ public abstract class UIButton : MonoBehaviour
 	[field: SerializeField]
 	public List<UISwap> swapList { get; private set; }
 
-	public int layerOrder = 0;
-
 	protected abstract void ActiveFunc();
 
 	public void Active()
