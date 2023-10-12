1f105876b2592fe0479facf4a0f9ef3bb1ab2e1a
rbase0218 <rbase@naver.com>
2023-10-12 15:26:31 +0900

    Fix : UIButton 이벤트를 통해서 기능 분리

--- Assets/00Scripts/03UI/Part/UIPassivePartSelect.cs
+++ Assets/00Scripts/03UI/Part/UIPassivePartSelect.cs
@@ -5,8 +5,45 @@ using UnityEngine;
 public class UIPassivePartSelect : MonoBehaviour
 {
 	public List<UIPartSelectButton> partSelectButtons;
+	
+	[field: SerializeField, Space(15)]
+	public UIPartEquip equip { get; private set; }
 
-	// Part Item Code를 받아서 ChangeResources를 시킨다.
-	// ChangeResources()
+	private void Awake()
+	{
+		for (int i = 0; i < partSelectButtons.Count; ++i)
+		{
+			partSelectButtons[i].onActive?.AddListener(OpenEquipWindow);
+			Debug.Log("ADD");
+		}
+	}
+
+	// 해당 메서드를  통해서 Passive Select Part 정리.
+	public void SetPartData(int[] partCodes)
+	{
+		for (int i = 0; i < partSelectButtons.Count; ++i)
+		{
+			partSelectButtons[i].SetButtonData(partCodes[i], i);
+		}
+	}
+
+	private void OpenEquipWindow(int code, int index)
+	{
+		// Equip System에 선택 파츠 넘겨주기
+		equip.SetSelectPart(code);
+		
+		for (int i = 0; i < partSelectButtons.Count; ++i)
+		{
+			partSelectButtons[i].onActive.RemoveAllListeners();
+		}
+
+		// 닫기
+		UIManager.Instance.CloseWindow(WindowList.PASSIVE_PART);
+		
+		// 열기
+		UIManager.Instance.OpenWindow(WindowList.PART_EQUIP);
+
+		
+	}
 
 }
