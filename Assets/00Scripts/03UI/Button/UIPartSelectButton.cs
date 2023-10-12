65c47322352d6d580eccb0dededd3399ed22589a
rbase0218 <rbase@naver.com>
2023-10-12 15:25:57 +0900

    Fix : Addressable을 통해서 Data Load 기능 추가 및 선택 데이터 연동

--- Assets/00Scripts/03UI/Button/UIPartSelectButton.cs
+++ Assets/00Scripts/03UI/Button/UIPartSelectButton.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Events;
 
 public class UIPartSelectButton : UIButton
 {
+	[SerializeField]
+	private int partCode;
+
+	[SerializeField]
+	private bool isSelectMode = false;
+
+	private int buttonIndex = 0;
+	
 	[field: SerializeField]
 	public Image PartIconImage { get; private set; }
 
@@ -18,12 +28,33 @@ public class UIPartSelectButton : UIButton
 	[field: SerializeField]
 	public TMP_Text SubInfoText { get; private set; }
 
+	[HideInInspector]
+	public UnityEvent<int, int> onActive;
+
 	protected override void ActiveFunc()
 	{
-
+		// 선택이 되었을 때
+		onActive?.Invoke(partCode, buttonIndex);
 	}
 
-	public void ChangeResource(UIPassiveSelectData selectData)
+	public void SetButtonData(int code, int index)
+	{
+		ChangeResource(LoadPartData(code));
+		SetIndex(index);
+	}
+
+	private void SetIndex(int index)
+	{
+		buttonIndex = index;
+	}
+
+	private UIPassiveSelectData LoadPartData(int code)
+	{
+		UIPassiveSelectData data = Addressables.LoadAssetAsync<UIPassiveSelectData>(code.ToString()).WaitForCompletion();
+		return data;
+	}
+
+	private void ChangeResource(UIPassiveSelectData selectData)
 	{
 		PartIconImage.sprite = selectData.partIconSpr;
 		PartNameImage.sprite = selectData.partNameSpr;
