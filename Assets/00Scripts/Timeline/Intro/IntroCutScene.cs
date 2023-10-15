598c63283446414dc626d2c69ce532667c26358a
Hyoung-Seok <dkavmdk98@gmail.com>
2023-10-16 02:42:39 +0900

    Fix : Intro 컷씬 수정

--- Assets/00Scripts/Timeline/Intro/IntroCutScene.cs
+++ Assets/00Scripts/Timeline/Intro/IntroCutScene.cs
@@ -7,12 +7,7 @@ using UnityEngine.Playables;
 public class IntroCutScene : CutSceneBase
 {
 	[Header("Component")] 
-	[SerializeField] private PlayableDirector introCurScene;
-	[SerializeField] private TextMeshProUGUI inputField;
-	
-	[Header("스크립트 데이터")]
-	[SerializeField] private List<ScriptingList> scriptsList;
-	private int curScriptsIndex = 0;
+	[SerializeField] private PlayableDirector introCutScene;
 	
 	[Header("Fade Time")] 
 	[SerializeField] private float fadeInTime = 0.8f;
@@ -41,13 +36,6 @@ public class IntroCutScene : CutSceneBase
 	
 	public void Intro_PrintScripts()
 	{
-		introCurScene.Pause();
-
-		chapterManager.PauseCutSceneUntilScriptsEnd(introCurScene);
-		chapterManager.scripting.StartPrintingScript(scriptsList[curScriptsIndex].scriptList);
-	
-		curScriptsIndex = (curScriptsIndex + 1 < scriptsList.Count) ? curScriptsIndex + 1 : 0;
+		
 	}
-
-	public void ClearInputField() => inputField.text = "";
 }
