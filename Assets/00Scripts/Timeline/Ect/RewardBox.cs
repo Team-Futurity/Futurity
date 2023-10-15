3557632859635b6b8cc2036e02c5fc5e2e0d7bc1
rbase0218 <rbase@naver.com>
2023-10-16 04:00:03 +0900

    Fix : 테스트용 Reward Box 수정

--- Assets/00Scripts/Timeline/Ect/RewardBox.cs
+++ Assets/00Scripts/Timeline/Ect/RewardBox.cs
@@ -5,22 +5,13 @@ using UnityEngine;
 
 public class RewardBox : MonoBehaviour
 {
-	public bool isEnable = false;
-
 	public UIPassivePartSelect passivePartSelect;
 	private void OnTriggerStay(Collider other)
 	{
-		if (isEnable == false)
-		{
-			return;
-		}
-		
 		if (other.CompareTag("Player"))
 		{
 			if (Input.GetKeyDown(KeyCode.F))
 			{
-				Debug.Log("??");
-
 				passivePartSelect.SetPartData(2101, 2102, 2103);
 				UIManager.Instance.OpenWindow(WindowList.PASSIVE_PART);
 
