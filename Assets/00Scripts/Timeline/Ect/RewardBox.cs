ae3812b673db32789f75addd05cff7c23fecc603
Budy123 <kth5948@naver.com>
2023-10-17 04:22:57 +0900

    Fix : 리워드 박스 상호작용 수정

--- Assets/00Scripts/Timeline/Ect/RewardBox.cs
+++ Assets/00Scripts/Timeline/Ect/RewardBox.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RewardBox : MonoBehaviour
 {
@@ -9,22 +10,38 @@ public class RewardBox : MonoBehaviour
 
 	public int[] partCodes;
 
+	private bool isEnter;
+
 	private void Start()
 	{
 		InputActionManager.Instance.ToggleActionMap(InputActionManager.Instance.InputActions.Player);
 	}
 
-	private void OnTriggerStay(Collider other)
+	private void OnTriggerEnter(Collider other)
 	{
 		if (other.CompareTag("Player"))
 		{
-			if (Input.GetKeyDown(KeyCode.T) && !UIManager.Instance.IsOpenWindow(WindowList.PASSIVE_PART))
-			{
-				passivePartSelect.SetPartData(partCodes);
-				UIManager.Instance.OpenWindow(WindowList.PASSIVE_PART);
-
-				InputActionManager.Instance.ToggleActionMap(InputActionManager.Instance.InputActions.UIBehaviour);
-			}
+			isEnter = true;
+			InputActionManager.Instance.RegisterCallback(InputActionManager.Instance.InputActions.Player.Interaction, OnInteractRewardBox, true);
 		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			isEnter = false;
+			InputActionManager.Instance.RemoveCallback(InputActionManager.Instance.InputActions.Player.Interaction, OnInteractRewardBox, true);
+		}
+	}
+
+	public void OnInteractRewardBox(InputAction.CallbackContext context)
+	{
+		if (UIManager.Instance.IsOpenWindow(WindowList.PASSIVE_PART)) { return; }
+
+		passivePartSelect.SetPartData(partCodes);
+		UIManager.Instance.OpenWindow(WindowList.PASSIVE_PART);
+
+		InputActionManager.Instance.ToggleActionMap(InputActionManager.Instance.InputActions.UIBehaviour);
 	}
 }
