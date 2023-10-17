eff2e56924d1458f1f1eb7b8f6153b32d2711712
Hyoung-Seok <dkavmdk98@gmail.com>
2023-10-17 10:03:15 +0900

    Fix : Reward박스 사라지도록 변경 및 코드 수정

--- Assets/00Scripts/Timeline/Ect/RewardBox.cs
+++ Assets/00Scripts/Timeline/Ect/RewardBox.cs
@@ -7,41 +7,44 @@ using UnityEngine.InputSystem;
 public class RewardBox : MonoBehaviour
 {
 	public UIPassivePartSelect passivePartSelect;
-
 	public int[] partCodes;
-
 	private bool isEnter;
-
-	private void Start()
-	{
-		InputActionManager.Instance.ToggleActionMap(InputActionManager.Instance.InputActions.Player);
-	}
-
+	
 	private void OnTriggerEnter(Collider other)
 	{
-		if (other.CompareTag("Player"))
+		if (other.CompareTag("Player") == false)
 		{
-			isEnter = true;
-			InputActionManager.Instance.RegisterCallback(InputActionManager.Instance.InputActions.Player.Interaction, OnInteractRewardBox, true);
+			return;
 		}
+		
+		isEnter = true;
+		InputActionManager.Instance.RegisterCallback(InputActionManager.Instance.InputActions.Player.Interaction, 
+			OnInteractRewardBox, true);
 	}
 
 	private void OnTriggerExit(Collider other)
 	{
-		if (other.CompareTag("Player"))
+		if (other.CompareTag("Player") == false)
 		{
-			isEnter = false;
-			InputActionManager.Instance.RemoveCallback(InputActionManager.Instance.InputActions.Player.Interaction, OnInteractRewardBox, true);
+			return;
 		}
+		
+		isEnter = false;
+		InputActionManager.Instance.RemoveCallback(InputActionManager.Instance.InputActions.Player.Interaction, 
+			OnInteractRewardBox, true);
 	}
 
-	public void OnInteractRewardBox(InputAction.CallbackContext context)
+	private void OnInteractRewardBox(InputAction.CallbackContext context)
 	{
-		if (UIManager.Instance.IsOpenWindow(WindowList.PASSIVE_PART)) { return; }
+		if (UIManager.Instance.IsOpenWindow(WindowList.PASSIVE_PART))
+		{
+			return;
+		}
 
 		passivePartSelect.SetPartData(partCodes);
 		UIManager.Instance.OpenWindow(WindowList.PASSIVE_PART);
 
 		InputActionManager.Instance.ToggleActionMap(InputActionManager.Instance.InputActions.UIBehaviour);
+		gameObject.SetActive(false);
 	}
 }
