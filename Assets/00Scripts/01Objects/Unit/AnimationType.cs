d2c8433010cfe3803e0b9f495c3045db030bf390
Budy123 <kth5948@naver.com>
2023-10-22 15:59:09 +0900

    Remove : 불필요한 루트모션 코드 삭제

--- Assets/00Scripts/01Objects/Unit/AnimationType.cs
+++ Assets/00Scripts/01Objects/Unit/AnimationType.cs
@@ -6,7 +6,4 @@ public class AnimationType
 {
 	public string animationName;
 	public bool isRootMotion;
-	[Range(0,1)] public int applyX;
-	[Range(0, 1)] public int applyY;
-	[Range(0, 1)] public int applyZ;
 }
