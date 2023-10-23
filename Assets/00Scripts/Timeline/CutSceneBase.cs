0e5fa608d51e1026cce9a6423b4ca3117d2cf373
Hyoung-Seok <dkavmdk98@gmail.com>
2023-10-23 19:51:13 +0900

    Style : 접근 제한자 변경

--- Assets/00Scripts/Timeline/CutSceneBase.cs
+++ Assets/00Scripts/Timeline/CutSceneBase.cs
@@ -6,7 +6,7 @@ public abstract class CutSceneBase : MonoBehaviour
 	[SerializeField] protected ChapterCutSceneManager chapterManager;
 	protected virtual void Init() { }
 	protected virtual void EnableCutScene() { }
-	public abstract void DisableCutScene();
+	protected abstract void DisableCutScene();
 
 	private void OnEnable()
 	{
