257c0de6c5e745d9ef60cc3a7e05474ba26b2ca5
rbase0218 <rbase@naver.com>
2023-10-26 19:22:38 +0900

    Fix : Dialog Directing Player 로직 순환 구현

--- Assets/00Scripts/03UI/Dialog/Directing/DialogDirectingPlayer.cs
+++ Assets/00Scripts/03UI/Dialog/Directing/DialogDirectingPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DialogDirectingPlayer : MonoBehaviour
 {
@@ -9,6 +10,18 @@ public class DialogDirectingPlayer : MonoBehaviour
 
 	private UIPerformBoardHandler performHandler;
 
+	private DialogData currentDialog;
+
+	private List<int> saveDialogID = new List<int>();
+
+	#region Unity Events
+
+	public UnityEvent onPlay;
+	public UnityEvent onEnded;
+
+	#endregion
+
+	// Perform Instance를 등록해준다.
 	public void SetData(UIPerformBoardHandler handler)
 	{
 		performHandler = handler;
@@ -16,19 +29,70 @@ public class DialogDirectingPlayer : MonoBehaviour
 
 	public void Play()
 	{
+		if (performHandler == null)
+		{
+			FDebug.Log($"Perform Handler를 등록해야 함", GetType());
+		}
 
+		onPlay?.Invoke();
+
+		UpdateAction();
 	}
 
 	private void UpdateAction()
 	{
-		foreach(var source in directingSource)
+		DialogDirectingSource lastSource = new DialogDirectingSource();
+		
+		foreach (var source in directingSource)
 		{
-			
+			var dialogSource = source.dialogSource;
+			var eventType = source.eventType;
+
+			// Current Dialog가 비어있거나, 지금과 동일하지 않을 경우 새로 추가한다.
+			if (currentDialog == null)
+			{
+				currentDialog = dialogSource;
+
+				saveDialogID.Add(currentDialog.GetInstanceID());
+			}
+			else if (currentDialog != dialogSource)
+			{
+				bool beforeSave = saveDialogID.Contains(currentDialog.GetInstanceID());
+
+				if (!beforeSave)
+				{
+					currentDialog.onInit?.AddListener(() =>
+					{
+						performHandler.OpenPerform(0);
+					});
+
+					currentDialog.onEnded?.AddListener(() =>
+					{
+						performHandler.OpenPerform(1);
+					});
+				}
+
+				// Change
+				currentDialog = dialogSource;
+			}
+
+			switch (eventType)
+			{
+				case DialogDirectingSource.DialogEventType.START:
+					performHandler.AddPerformBoard(0, source.board);
+					break;
+
+				case DialogDirectingSource.DialogEventType.END:
+					performHandler.AddPerformBoard(1, source.board);
+					break;
+			}
+
+			lastSource = source;
 		}
+
+		lastSource.board.onEndedAction += () =>
+		{
+			onEnded?.Invoke();
+		};
 	}
-
-	// 순회하면서 Event를 등록을 해준다.
-
-	// Perform의 Change의 경우
-
 }
