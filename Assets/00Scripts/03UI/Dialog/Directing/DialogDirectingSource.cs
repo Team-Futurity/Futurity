2b5ec1cb2efbebba7aaf35d0516bb59f95a694b5
rbase0218 <rbase@naver.com>
2023-10-26 19:53:17 +0900

    Remove : 불필요한 State 제거

--- Assets/00Scripts/03UI/Dialog/Directing/DialogDirectingSource.cs
+++ Assets/00Scripts/03UI/Dialog/Directing/DialogDirectingSource.cs
@@ -9,9 +9,7 @@ public class DialogDirectingSource
 	{
 		NONE,
 
-		BEFORE,
 		START,
-		STAY,
 		END,
 
 		MAX
@@ -19,6 +17,7 @@ public class DialogDirectingSource
 	// Source	: 어떤 Dialog에서 실행을 할 것인가?
 	// Event	: Dialog의 어떤 이벤트에서 실행을 할 것인가?
 	// Action	: 해당하는 Source의 Event에서 어떤 Action을 실행할 것인가? -> 지금은 Perfrom만
+	//			  무조건 Open 아니면 안됨..ㅎ
 
 	public DialogData dialogSource;
 	public DialogEventType eventType;
