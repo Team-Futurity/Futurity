4551fb7bd190461fbd563b2f03302b014a052e90
rbase0218 <rbase@naver.com>
2023-10-27 09:28:54 +0900

    Fix : 변경 시점을 알기 위한 Type 추가

--- Assets/00Scripts/03UI/Dialog/Directing/DialogDirectingSource.cs
+++ Assets/00Scripts/03UI/Dialog/Directing/DialogDirectingSource.cs
@@ -10,15 +10,13 @@ public class DialogDirectingSource
 		NONE,
 
 		START,
+		NEXT_CHANGE_START,
 		END,
+		NEXT_CHANGE_END,
 
 		MAX
 	}
-	// Source	: 어떤 Dialog에서 실행을 할 것인가?
-	// Event	: Dialog의 어떤 이벤트에서 실행을 할 것인가?
-	// Action	: 해당하는 Source의 Event에서 어떤 Action을 실행할 것인가? -> 지금은 Perfrom만
-	//			  무조건 Open 아니면 안됨..ㅎ
-
+	
 	public DialogData dialogSource;
 	public DialogEventType eventType;
 	public UIPerformBoard board;
