02de95d95893709ca8debe9b1853df88c4638d0d
rbase0218 <rbase@naver.com>
2023-10-29 01:25:04 +0900

    Fix : Tutorial Dialog 변경으로 인한 코드 제거

--- Assets/00Scripts/00System/Tutorial/TutorialController.cs
+++ Assets/00Scripts/00System/Tutorial/TutorialController.cs
@@ -6,24 +6,17 @@ using UnityEngine.AddressableAssets;
 
 public class TutorialController : MonoBehaviour
 {
-	[SerializeField] private float fadeTime = 1f;
+	[SerializeField]
+	private float fadeTime = 1f;
 
-	[SerializeField] private UIPerformBoardHandler performHandler;
-	[SerializeField] private UIDialogController dialogController;
+	[SerializeField]
+	private UIPerformBoardHandler performHandler;
 
-	
 	// Dialog Data Set
 	private List<DialogData> tutorialDialogList = new List<DialogData>();
-	private readonly string dialogPath = "Assets/04Data/Dialog/Tutorial/";
-	private readonly string[] dialogKey =
-	{
-		"TutorialData1",
-		"TutorialData2",
-		"TutorialData3"
-	};
 
 	// Only Debug
-	[Space(10), Header("Debug ��� ����")]
+	[Space(10), Header("Debug Mode")]
 	public bool isDebugMode = false;
 
 	private void Awake()
@@ -32,97 +25,10 @@ public class TutorialController : MonoBehaviour
 		{
 			InputActionManager.Instance.ToggleActionMap(InputActionManager.Instance.InputActions.Player);
 		}
-
-		LoadTutorialDialogData();
 	}
 
 	private void Start()
 	{
-		if (isDebugMode)
-		{
-			StartTutorial();
-		}
-		else
-		{
-			FadeManager.Instance.FadeOut(fadeTime, StartTutorial);
-		}
-	}
-
-	private void StartTutorial()
-	{
-		//// First Settings
-		//dialogController.SetDialogData(tutorialDialogList[currentDialogIndex]);
-		
-		//// Event - Dialog Ended
-		//dialogController.OnEnded?.AddListener(() =>
-		//{
-		//	switch (currentDialogIndex)
-		//	{
-		//		case 0:
-		//			performHandler.SetPerfrom();
-		//			performHandler.Run();
-		//			break;
-
-		//		case 1:
-		//			performHandler.ChangeToNextBoard();
-		//			break;
-		//	}
-		//});
-
-		//// Event - Perform Changed
-		//performHandler.OnChangePerformBoard?.AddListener(() =>
-		//{
-		//	switch (currentDialogIndex)
-		//	{
-		//		case 0:
-		//			currentDialogIndex++;
-		//			dialogController.SetDialogData(tutorialDialogList[currentDialogIndex]);
-					
-		//			dialogController.PlayDialog();
-		//			break;
-				
-		//		case 1:
-		//			performHandler.ChangeToNextBoard();
-		//			currentDialogIndex++;
-		//			break;
-		//	}
-		//});
-		
-		//// Event - Perform Ended
-		//performHandler.OnEnded?.AddListener(() =>
-		//{
-		//	dialogController.SetDialogData(tutorialDialogList[maxDialogIndex]);
-		//	dialogController.PlayDialog();
-			
-		//	dialogController.OnEnded?.RemoveAllListeners();
-		//	dialogController.OnEnded?.AddListener(() =>
-		//	{
-		//		if (isDebugMode)
-		//		{
-		//			SceneLoader.Instance.LoadScene("Chapter1-Stage1");
-		//		}
-		//		else
-		//		{
-		//			FadeManager.Instance.FadeIn(fadeTime, () =>
-		//			{
-		//				SceneLoader.Instance.LoadScene("Chapter1-Stage1");
-		//			});
-		//		}
-		//	});
-		//});
-		
-		//// Play
-		//dialogController.PlayDialog();
-	}
-
-	private void LoadTutorialDialogData()
-	{
-		foreach (var key in dialogKey)
-		{
-			var path = dialogPath + key + ".asset";
-
-			DialogData dialogData = Addressables.LoadAsset<DialogData>(path).WaitForCompletion();
-			tutorialDialogList.Add(dialogData);
-		}
 	}
 }
+
