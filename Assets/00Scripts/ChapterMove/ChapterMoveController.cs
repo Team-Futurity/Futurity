34973e224192172b9a0998915266151c2df5d7b9
Hyoung-Seok <dkavmdk98@gmail.com>
2023-10-28 19:29:45 +0900

    Remove : Debug 처리 코드 제거

--- Assets/00Scripts/ChapterMove/ChapterMoveController.cs
+++ Assets/00Scripts/ChapterMove/ChapterMoveController.cs
@@ -4,34 +4,28 @@ using UnityEngine;
 
 public class ChapterMoveController : MonoBehaviour
 {
-	[Header("Component")] 
-	[SerializeField] private GameObject interactionUI;
+	[Header("Component")] [SerializeField] private GameObject interactionUI;
 	public void SetActiveInteractionUI(bool isActive) => interactionUI.SetActive(isActive);
 	[ReadOnly(false), SerializeField] private ChapterCutSceneManager cutSceneManager;
 
-	[Header("챕터 정보")] 
-	[SerializeField] private List<ChapterData> chapterData;
+	[Header("챕터 정보")] [SerializeField] private List<ChapterData> chapterData;
 	[SerializeField, ReadOnly(false)] private EChapterType curChapter = EChapterType.CHAPTER1_1;
 
-	[Header("Fade Out 시간")] 
-	[SerializeField] private float fadeOutTime = 0.5f;
+	[Header("Fade Out 시간")] [SerializeField]
+	private float fadeOutTime = 0.5f;
+
 	[SerializeField] private float fadeInTime = 1.0f;
 
-	[Header("다음 씬으로 넘어갈 콜라이더")] 
-	[SerializeField] private GameObject chapterMoveTrigger;
-	
-	[Header("디버그용 패널")] 
-	[SerializeField] private bool isDebugMode;
-	[SerializeField] private bool enableSpawner;
-	[SerializeField] private List<SpawnerManager> spawnerManager;
-	
+	[Header("다음 씬으로 넘어갈 콜라이더")] [SerializeField]
+	private GameObject chapterMoveTrigger;
+
 	private void Start()
 	{
 		Init();
-		CheckDebugMode();
 		EnableEntryCutScene();
 
-		GameObject.FindWithTag("Player").GetComponent<PlayerController>().playerData.status.updateHPEvent.Invoke(230f, 230f);
+		GameObject.FindWithTag("Player").GetComponent<PlayerController>().playerData.status
+			.updateHPEvent.Invoke(230f, 230f);
 		Time.timeScale = 1.0f;
 	}
 
@@ -42,20 +36,21 @@ public class ChapterMoveController : MonoBehaviour
 			MoveNextChapter();
 		}
 	}
-	
+
 	public void EnableExitCollider()
 	{
 		if (chapterMoveTrigger == null)
 		{
 			return;
 		}
+
 		chapterMoveTrigger.SetActive(true);
 	}
 
 	public void MoveNextChapter()
 	{
 		InputActionManager.Instance.DisableActionMap();
-		
+
 		FadeManager.Instance.FadeIn(fadeInTime, () =>
 		{
 			SceneLoader.Instance.LoadScene(chapterData[(int)curChapter].NextChapterName);
@@ -66,21 +61,21 @@ public class ChapterMoveController : MonoBehaviour
 	private void EnableEntryCutScene()
 	{
 		int index = (int)curChapter;
-		if (isDebugMode == true && chapterData[index].CutSceneType == ECutSceneType.NONE)
+		if (chapterData[index].CutSceneType == ECutSceneType.NONE)
 		{
 			FadeManager.Instance.FadeOut(chapterData[index].FadeOutTime);
 			return;
 		}
 
 		Action cutSceneEvent = null;
-		
+
 		TimelineManager.Instance.EnableCutScene(chapterData[index].CutSceneType);
 
 		cutSceneEvent = () =>
 		{
 			TimelineManager.Instance.EnableNonPlayOnAwakeCutScene(chapterData[index].CutSceneType);
 		};
-		
+
 		FadeManager.Instance.FadeOut(fadeOutTime, () => cutSceneEvent?.Invoke());
 	}
 
@@ -91,26 +86,4 @@ public class ChapterMoveController : MonoBehaviour
 			cutSceneManager.InitManager();
 		}
 	}
-
-	#region OnlyUseEditor
-
-	private void CheckDebugMode()
-	{
-		if (isDebugMode == false)
-		{
-			return;
-		}
-		
-		if (enableSpawner == false)
-		{
-			return;
-		}
-
-		foreach (SpawnerManager manager in spawnerManager)
-		{
-			manager.SpawnEnemy();
-		}
-	}
-	
-	#endregion
 }
