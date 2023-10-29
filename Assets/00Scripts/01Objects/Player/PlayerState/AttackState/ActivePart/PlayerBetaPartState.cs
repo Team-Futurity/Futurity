a77d63fbe2950948e461051ef9885b697a762fe0
rbase0218 <rbase@naver.com>
2023-10-29 21:33:14 +0900

    Fix : Phase 1, 2에 해당하는 기능 구현 (이펙트 애니메이션 제외)

--- Assets/00Scripts/01Objects/Player/PlayerState/AttackState/ActivePart/PlayerBetaPartState.cs
+++ Assets/00Scripts/01Objects/Player/PlayerState/AttackState/ActivePart/PlayerBetaPartState.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 [FSMState((int)PlayerState.BetaSM)]
 public class PlayerBetaPartState : PlayerSpecialMoveState<BetaActivePart>
 {
+	private enum BetaProcess
+	{
+		NONE = 0,
+		FIRST_PHASE = 1,
+		SECOND_PHASE = 2,
+		THIRD_PHASE = 3,
+		END = 4
+	}
+
 	// Common
 	private List<UnitBase> enemyList = new List<UnitBase>();
 
@@ -13,13 +22,9 @@ public class PlayerBetaPartState : PlayerSpecialMoveState<BetaActivePart>
 	private TruncatedBoxCollider boxColl;
 	private TruncatedCapsuleCollider capsuleColl;
 
-	private ColliderBase currentCollider;
+	private BetaProcess currentProcess;
+	private bool isPlaying = false;
 
-	// Phase 1 -> 2 : Animation Ended
-	// Phase 2 -> 3 : Animation Ended
-	// Phase 3 : MaxSize
-	private int skillCycle = 0;
-	
 	// Phase 1
 	private float firstMaxAngle = .0f;
 	private float firstMinSize = .0f;
@@ -28,42 +33,42 @@ public class PlayerBetaPartState : PlayerSpecialMoveState<BetaActivePart>
 
 	private Transform firstEffect;
 
-	private readonly string FirstBetaAnimKey = "T";
-	
+	private readonly string FirstBetaAnimKey = "TestBetaAnim";
+
 	// Phase 2
 	private float secondMaxAngle = .0f;
 	private float secondMinSize = .0f;
 	private float secondRadius;
 	private float secondDamage = .0f;
-	
+
 	private readonly string SecondBetaAnimKey = "TT";
-	
+
 	// Phase 3
-	
-	
-	// Collider를 만들고 Player의 X축 중앙 값으로 Collider를 가져오는 것이 나을듯
-	
+
 	public override void Begin(PlayerController unit)
 	{
 		base.Begin(unit);
-		
+
+		Debug.Log("Beta State 진입");
+
 		// 혹시 모를 이전 데이터 제거
 		enemyList.Clear();
-		
-		// TimelineManager에서 CutScene Event 실행 (여기서)
-		
+
 		pc = unit;
 
-		firstMinSize = proccessor.firstRadius * MathPlus.cm2m;
-		
-		// 맨 처음 시작 시에는 1타 전용 
-		if (unit.attackColliderChanger.GetCollider(ColliderType.Box) is TruncatedCapsuleCollider capsuleCollider)
+		firstMinSize = proccessor.firstRadius;
+		secondRadius = proccessor.secondRadius;
+
+		if (unit.attackColliderChanger.GetCollider(ColliderType.Capsule) is TruncatedCapsuleCollider capsuleCollider)
 		{
 			capsuleColl = capsuleCollider;
-			
-			currentCollider = capsuleColl;
-			currentCollider.SetCollider(firstMaxAngle, firstMinSize);
+			capsuleColl.SetCollider(firstMaxAngle, firstMinSize);
 		}
+
+		capsuleColl.ColliderReference.enabled = true;
+		
+		currentProcess = BetaProcess.FIRST_PHASE;
+		isPlaying = true;
 	}
 
 	public override void Update(PlayerController unit)
@@ -73,84 +78,158 @@ public class PlayerBetaPartState : PlayerSpecialMoveState<BetaActivePart>
 		// 왜 쓴거지
 		if (currentTime == Time.deltaTime) return;
 
-		if (skillCycle == 1)
-		{
-			OnFirstPhase();			
-		}
-		else if (skillCycle == 2)
-		{
-			pc.ChangeState(PlayerState.Idle);
-		}
-		else if (skillCycle == 3)
-		{
-			// Phase 3
-		}
-		else if (skillCycle == 4)
-		{
-			// Ended
-		}
+		if (!isPlaying) return;
 		
-		
-		// 기점을 시작으로 진행하였음
+		switch (currentProcess)
+		{
+			case BetaProcess.NONE:
+				break;
+
+			case BetaProcess.FIRST_PHASE:
+				isPlaying = false;
+				
+				OnFirstPhase(unit);
+
+				break;
+
+			case BetaProcess.SECOND_PHASE:
+				isPlaying = false;
+				
+				OnSecondPhase(unit);
+
+				break;
+
+			case BetaProcess.THIRD_PHASE:
+				isPlaying = false;
+				
+				OnThirdPhase();
+
+				break;
+
+			case BetaProcess.END:
+				break;
+		}
 	}
 
 	public override void FixedUpdate(PlayerController unit)
 	{
-		
 	}
 
 	public override void End(PlayerController unit)
 	{
-		
+		Debug.Log("Beta State을 벗어남");
 	}
 
 	public override void OnTriggerEnter(PlayerController unit, Collider other)
 	{
-		
+		base.OnTriggerEnter(unit, other);
+
+		if (other.CompareTag(unit.EnemyTag))
+		{
+			other.TryGetComponent<UnitBase>(out var enemy);
+			if (enemy == null) { return; }
+
+			enemyList.Add(enemy);
+		}
 	}
 
 	// 이펙트 동시에 데미지 처리
-	private void OnFirstPhase()
+	private void OnFirstPhase(PlayerController unit)
 	{
+		Debug.Log("On First Phase");
+		
 		// Collider Open
-		currentCollider.SetCollider(firstMaxAngle, firstRadius);
+		capsuleColl.SetCollider(firstMaxAngle, firstMinSize);
 		// Effect Create
+		// unit.animator.SetBool(FirstBetaAnimKey, true);
 
 		// Damage Add
-		AddDamageEnemy(pc);
+		AddDamageEnemy(unit);
 		
-
-		skillCycle += 1;
+		OnFirstPhaseEnded(unit);
 	}
 
-	private void OnSecondPhase()
+	private void OnFirstPhaseEnded(PlayerController unit)
 	{
-		currentCollider.SetCollider(secondMaxAngle, secondRadius);
+		// Next Process Set
+		currentProcess = BetaProcess.SECOND_PHASE;
+		unit.animator.SetBool(FirstBetaAnimKey, false);
 		
-		skillCycle += 1;
+		capsuleColl.SetCollider(0, 0);
+		
+		isPlaying = true;
+	}
+
+	private void OnSecondPhase(PlayerController unit)
+	{
+		Debug.Log("On Second Phase");
+		
+		// Collider
+		capsuleColl.SetCollider(secondMaxAngle, secondRadius);
+		
+		// Animation
+		// Effect
+
+		// Damage Add
+		AddDamageEnemy(unit);
+		
+		OnSecondPhaseEnded(unit);
+	}
+
+	private void OnSecondPhaseEnded(PlayerController unit)
+	{
+		// Next Process Set
+		currentProcess = BetaProcess.THIRD_PHASE;
+
+		isPlaying = true;
 	}
 
 	private void OnThirdPhase()
 	{
-		//currentCollider.setC
-		skillCycle += 1;
+		Debug.Log("On Third Phase");
+		// Collider 늘어나게 하기 
+		// Collider 방향을 플레이어가 바라보는 방향으로 설정하기
+		
+		// 실행 시간 지났을 경우, 데이터 Max로 세팅하기
+		// Collider 적용.
+		// 데미지 전달
+		
+		// 애니메이션 끝났을 경우, OnThirdPhaseEnded 호출하기
+	}
+
+	private void OnThirdPhaseEnded()
+	{
+		// Next Process Set
+		currentProcess = BetaProcess.END;
+		
+	}
+
+	private void OnEnded()
+	{
+		pc.ChangeState(PlayerState.Idle);
 	}
 
 	// Enemy에게 데미지 전달
 	private void AddDamageEnemy(PlayerController unit)
 	{
+		Debug.Log("Add Damage Enemy Start");
+		
+		Debug.Log(enemyList.Count);
+		
 		foreach (var enemy in enemyList)
 		{
 			DamageInfo info = new DamageInfo(unit.playerData, enemy, 1);
 
 			// Cycle 마다 데미지가 다르기 때문
-			var currentDamage = 
-				(skillCycle == 1) ? firstDamage : (skillCycle == 2) ? secondDamage : 30;
+			var currentDamage = currentProcess == BetaProcess.FIRST_PHASE ? firstDamage :
+				currentProcess == BetaProcess.SECOND_PHASE ? secondDamage : 300;
+			
+			Debug.Log(currentProcess);
 			
 			info.SetDamage(currentDamage);
 			enemy.Hit(info);
 		}
-		
+
 		enemyList.Clear();
 	}
 }
