97368463ced671c95e50af38754a68c246f25782
Hyoung-Seok <dkavmdk98@gmail.com>
2023-11-01 14:55:15 +0900

    Feat : 상호작용 스크립트 구현

---
+++ Assets/00Scripts/01Objects/InteractionTrigger.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class InteractionTrigger : MonoBehaviour
+{
+	[Header("Component")]
+	[SerializeField] private GameObject interactionUI;
+	[SerializeField] private UnityEvent interactionEvent;
+	private bool isInput = false;
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player") == false)
+		{
+			return;
+		}
+		
+		interactionUI.SetActive(true);
+		InputActionManager.Instance.RegisterCallback(InputActionManager.Instance.InputActions.Player.Interaction, InputCheck);
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.CompareTag("Player") == false)
+		{
+			return;
+		}
+
+		if (isInput == false)
+		{
+			return;
+		}
+		
+		interactionUI.SetActive(false);
+		InputActionManager.Instance.RemoveCallback(InputActionManager.Instance.InputActions.Player.Interaction, InputCheck);
+		
+		interactionEvent?.Invoke();
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		interactionUI.SetActive(false);
+		InputActionManager.Instance.RemoveCallback(InputActionManager.Instance.InputActions.Player.Interaction, InputCheck);
+	}
+
+	private void InputCheck(InputAction.CallbackContext context)
+	{
+		isInput = true;
+	}
+}
