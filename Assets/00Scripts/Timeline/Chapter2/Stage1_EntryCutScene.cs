56bc9468afa9b81a38422c5e16f5a4a9b2c7cf94
Hyoung-Seok <dkavmdk98@gmail.com>
2023-11-01 14:54:18 +0900

    Feat : 2c-1s 진입 컷씬 구조 생성

---
+++ Assets/00Scripts/Timeline/Chapter2/Stage1_EntryCutScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Stage1_EntryCutScene : CutSceneBase
+{
+	[Header("Component")] 
+	[SerializeField] private PlayableDirector cutScene;
+	
+	protected override void Init()
+	{
+		
+	}
+
+	protected override void EnableCutScene()
+	{
+		
+	}
+
+	protected override void DisableCutScene()
+	{
+		
+	}
+}
