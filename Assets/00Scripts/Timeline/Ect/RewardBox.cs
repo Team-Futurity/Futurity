fe7a2f0da76004015213c3e0f2c77cf9f6770006
Hyoung-Seok <dkavmdk98@gmail.com>
2023-11-01 23:12:13 +0900

    Feat : 박스 Open 애니메이션 제공

--- Assets/00Scripts/Timeline/Ect/RewardBox.cs
+++ Assets/00Scripts/Timeline/Ect/RewardBox.cs
@@ -6,13 +6,27 @@ using UnityEngine.InputSystem;
 
 public class RewardBox : MonoBehaviour
 {
+	[Header("부품 컴포넌트")]
 	public UIPassivePartSelect passivePartSelect;
 	public int[] partCodes;
 	private bool isEnter;
 
+	[Header("Open Animation")] 
+	[SerializeField] private Animation boxAnimations;
+	[SerializeField] private float waitTime = 0.6f;
+	private IEnumerator startAnimation;
+
 	private void OnDisable()
 	{
 		InputActionManager.Instance.RemoveCallback(InputActionManager.Instance.InputActions.Player.Interaction, OnInteractRewardBox, true);
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+			StartAnimation();
+		}
 	}
 
 	private void OnTriggerEnter(Collider other)
@@ -43,11 +57,26 @@ public class RewardBox : MonoBehaviour
 		{
 			return;
 		}
-
+		
 		passivePartSelect.SetPartData(partCodes);
 		UIManager.Instance.OpenWindow(WindowList.PASSIVE_PART);
 
 		InputActionManager.Instance.ToggleActionMap(InputActionManager.Instance.InputActions.UIBehaviour);
 		gameObject.SetActive(false);
+	}
+
+	private void StartAnimation()
+	{
+		startAnimation = PlayAnimation();
+		StartCoroutine(startAnimation);
+	}
+
+	private IEnumerator PlayAnimation()
+	{
+		InputActionManager.Instance.ToggleActionMap(InputActionManager.Instance.InputActions.UIBehaviour);
+		boxAnimations.Play();
+		
+		yield return new WaitForSeconds(waitTime);
+		Debug.Log("Open Done");
 	}
 }
