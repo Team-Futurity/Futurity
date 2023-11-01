8aa7f56a7a8c9c62ee7f1c017505472cab9cbc13
Hyoung-Seok <dkavmdk98@gmail.com>
2023-11-01 15:58:50 +0900

    Feat : 2c1s 진입 연출 스크립트 구현

--- Assets/00Scripts/Timeline/Chapter2/Stage1_EntryCutScene.cs
+++ Assets/00Scripts/Timeline/Chapter2/Stage1_EntryCutScene.cs
@@ -7,7 +7,12 @@ public class Stage1_EntryCutScene : CutSceneBase
 {
 	[Header("Component")] 
 	[SerializeField] private PlayableDirector cutScene;
-	
+	[SerializeField] private SpawnerManager spawnerManager;
+
+	[Header("스크립트 데이터")] 
+	[SerializeField] private List<ScriptingList> scriptingList;
+	private int curScriptsIndex;
+
 	protected override void Init()
 	{
 		
@@ -15,11 +20,24 @@ public class Stage1_EntryCutScene : CutSceneBase
 
 	protected override void EnableCutScene()
 	{
-		
+		chapterManager.SetActiveMainUI(false);
 	}
 
 	protected override void DisableCutScene()
 	{
+		spawnerManager.SpawnEnemy();
 		
+		chapterManager.scripting.DisableAllNameObject();
+		chapterManager.scripting.ResetEmotion();
+	}
+
+	public void C2A1_Scripting()
+	{
+		cutScene.Pause();
+		
+		chapterManager.PauseCutSceneUntilScriptsEnd(cutScene);
+		chapterManager.scripting.StartPrintingScript(scriptingList[curScriptsIndex].scriptList);
+		
+		curScriptsIndex = (curScriptsIndex + 1 < scriptingList.Count) ? curScriptsIndex + 1 : 0;
 	}
 }
