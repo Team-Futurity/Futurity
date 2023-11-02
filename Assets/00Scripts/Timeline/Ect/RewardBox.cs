45db97795d9a8f73c9644b2de743c8d7fe068977
Hyoung-Seok <dkavmdk98@gmail.com>
2023-11-02 11:58:02 +0900

    Fix : 애니메이션이 끝난 후 UI창 열리도록 변경

--- Assets/00Scripts/Timeline/Ect/RewardBox.cs
+++ Assets/00Scripts/Timeline/Ect/RewardBox.cs
@@ -9,26 +9,13 @@ public class RewardBox : MonoBehaviour
 	[Header("부품 컴포넌트")]
 	public UIPassivePartSelect passivePartSelect;
 	public int[] partCodes;
-	private bool isEnter;
 
 	[Header("Open Animation")] 
 	[SerializeField] private Animation boxAnimations;
 	[SerializeField] private float waitTime = 0.6f;
 	private IEnumerator startAnimation;
-
-	private void OnDisable()
-	{
-		InputActionManager.Instance.RemoveCallback(InputActionManager.Instance.InputActions.Player.Interaction, OnInteractRewardBox, true);
-	}
-
-	private void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.T))
-		{
-			StartAnimation();
-		}
-	}
-
+	
+	
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.CompareTag("Player") == false)
@@ -36,7 +23,6 @@ public class RewardBox : MonoBehaviour
 			return;
 		}
 		
-		isEnter = true;
 		InputActionManager.Instance.RegisterCallback(InputActionManager.Instance.InputActions.Player.Interaction, OnInteractRewardBox, true);
 	}
 
@@ -47,36 +33,29 @@ public class RewardBox : MonoBehaviour
 			return;
 		}
 		
-		isEnter = false;
 		InputActionManager.Instance.RemoveCallback(InputActionManager.Instance.InputActions.Player.Interaction, OnInteractRewardBox, true);
 	}
 
 	private void OnInteractRewardBox(InputAction.CallbackContext context)
 	{
-		if (UIManager.Instance.IsOpenWindow(WindowList.PASSIVE_PART))
-		{
-			return;
-		}
-		
-		passivePartSelect.SetPartData(partCodes);
-		UIManager.Instance.OpenWindow(WindowList.PASSIVE_PART);
-
-		InputActionManager.Instance.ToggleActionMap(InputActionManager.Instance.InputActions.UIBehaviour);
-		gameObject.SetActive(false);
-	}
-
-	private void StartAnimation()
-	{
 		startAnimation = PlayAnimation();
 		StartCoroutine(startAnimation);
 	}
-
+	
 	private IEnumerator PlayAnimation()
 	{
 		InputActionManager.Instance.ToggleActionMap(InputActionManager.Instance.InputActions.UIBehaviour);
 		boxAnimations.Play();
 		
 		yield return new WaitForSeconds(waitTime);
-		Debug.Log("Open Done");
+		
+		if (UIManager.Instance.IsOpenWindow(WindowList.PASSIVE_PART))
+		{ 
+			yield return null;
+		}
+		
+		passivePartSelect.SetPartData(partCodes);
+		UIManager.Instance.OpenWindow(WindowList.PASSIVE_PART);
+		gameObject.GetComponent<BoxCollider>().enabled = false;
 	}
 }
