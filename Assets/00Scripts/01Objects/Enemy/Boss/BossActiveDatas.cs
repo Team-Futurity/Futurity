52ade6d8d5e6b44f8e8229deb24c16588e104f43
j1nx09 <112684301+j1nx09@users.noreply.github.com>
2023-11-05 03:17:05 +0900

    Feat : Boss Phase/Attack Type Data

--- Assets/00Scripts/01Objects/Enemy/Boss/BossActiveDatas.cs
+++ Assets/00Scripts/01Objects/Enemy/Boss/BossActiveDatas.cs
@@ -5,27 +5,101 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BossActiveDatas", menuName = "ScriptableObject/Boss/BossActiveDatas")]
 public class BossActiveDatas : ScriptableObject
 {
-	public List<BossActiveData> activeDatas = new List<BossActiveData>();
+	public List<BossAttackData> attackDatas = new List<BossAttackData>();
 
-	public float GetAfterDelayValue(Phase phase, BossController.BossState skillType)
+	#region Set Value
+	public void SetTypePercentage(BossController unit, Phase phase, BossState skillType)
+	{
+		if (unit.curAttackData == null)
+			unit.curAttackData = new BossAttackData();
+
+		BossAttackData data = new BossAttackData();
+		foreach (var origin in attackDatas)
+			if (origin.phase == phase && origin.state == skillType)
+				data = origin;
+		unit.curAttackData = data;
+	}
+
+
+	#endregion
+
+	#region Get Value
+	public float GetAttackPointValue(Phase phase, BossState skillType)
 	{
 		float value = 0f;
 
-		foreach (var data in activeDatas)
-			if (data.phase == phase && data.skillType == skillType)
-				value = data.skillAfterDelay;
+		foreach (var data in attackDatas)
+			if (data.phase == phase && data.state == skillType)
+				value = data.extraAttackPoint;
+
+		return value;
+	}
+	public float GetPercentageValue(Phase phase, BossState skillType)
+	{
+		int value = 0;
+		phase = Phase.Phase1;
+
+		foreach (var data in attackDatas)
+			if (data.phase == phase && data.state == skillType)
+				value = data.percentage;
+
+		return value;
+	}
+	public float GetAttackDelayValue(Phase phase, BossState skillType)
+	{
+		float value = 0f;
+
+		foreach (var data in attackDatas)
+			if (data.phase == phase && data.state == skillType)
+				value = data.attackDelay;
 
 		return value;
 	}
 
-	public float GetActivateDelayValue(BossController.BossState skillType)
+	public float GetAttackSpeedValue(Phase phase, BossState skillType)
 	{
 		float value = 0f;
 
-		foreach (var data in activeDatas)
-			if (data.skillType == skillType)
-				value = data.skillActivateDelay;
+		foreach (var data in attackDatas)
+			if (data.phase == phase && data.state == skillType)
+				value = data.attackSpeed;
 
 		return value;
 	}
+
+	public float GetAfterDelayValue(Phase phase, BossState skillType)
+	{
+		float value = 0f;
+
+		foreach (var data in attackDatas)
+			if (data.phase == phase && data.state == skillType)
+				value = data.attackAfterDelay;
+
+		return value;
+	}
+	#endregion
+
+	#region Random Value
+
+	public void SetRandomNextState(BossController unit)
+	{
+		int sum = 0;
+		int random = Random.Range(1, 101);
+
+		foreach (var data in attackDatas)
+		{
+			if(data.phase == unit.curPhase)
+			{
+				sum += data.percentage;
+				if (random < sum)
+				{
+					while (data.state == unit.previousState)
+						SetRandomNextState(unit);
+					
+					unit.nextState = data.state;
+				}
+			}
+		}
+	}
+	#endregion
 }
---
+++ Assets/00Scripts/01Objects/Enemy/Boss/BossAttackData.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BossState : int
+{
+	SetUp,
+
+	Idle,
+	Chase,
+	Hit,
+	Death,
+
+	T0_Dash,
+	T1_Melee,
+	T2_Ranged,
+	T3_Move,
+	T3_Laser,
+	T4_Laser,
+	T5_EnemySpawn,
+	T6_Circle,
+}
+
+public enum Phase
+{
+	Phase1,
+	Phase2,
+}
+
+[Serializable]
+public class BossAttackData
+{
+	public Phase phase;
+	public BossState state;
+
+	public float extraAttackPoint;
+	public int percentage;
+
+	public float attackDelay;
+	public float attackSpeed;
+	public float attackAfterDelay;
+}
+
+[Serializable]
+public class BossPhaseData
+{
+	public Phase phase;
+	public int hpPercentage;
+}
