d86a4d5a88b6eff55454fac9aad07934cd8cda0b
rbase0218 <rbase@naver.com>
2023-11-06 23:42:29 +0900

    Fix : Credit Scene 퍼블리싱

---
+++ Assets/00Scripts/03UI/Credit/UICreditViewer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICreditViewer : MonoBehaviour
+{
+	public RectTransform creditTextRectTrans;
+	public Image bgImage;
+
+	public float duration = .0f;
+
+	private void Start()
+	{
+		// Credit 올라가는 속도와 BG Image가 끝까지 도달하는 시간은 동일해야 한다.
+	}
+
+	private void Update()
+	{
+		
+	}
+
+	private void StartCreditTextScroll()
+	{
+		
+	}
+
+	private void StartBgImageScroll()
+	{
+		
+	}
+	
+	
+
+
+}
