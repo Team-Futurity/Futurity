305eeff55713357de2d33aa5601d7c1dc8e790e3
Hyoung-Seok <dkavmdk98@gmail.com>
2023-11-06 19:14:02 +0900

    Style : 메소드 이름 변경

--- Assets/00Scripts/Timeline/Chapter1/Area1_EntryCutScene.cs
+++ Assets/00Scripts/Timeline/Chapter1/Area1_EntryCutScene.cs
@@ -1,5 +1,6 @@
 using Spine.Unity;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Playables;
@@ -9,10 +10,6 @@ public class Area1_EntryCutScene : CutSceneBase
 	[Header("Component")]
 	[SerializeField] private SpawnerManager spawnerManager;
 	[SerializeField] private PlayableDirector entryCutScene;
-	
-	[Header("플레이어 이동값")] 
-	[SerializeField] private GameObject targetPos;
-	[SerializeField] private float duration;
 
 	[Header("스크립트 데이터")] 
 	[SerializeField] private List<ScriptingList> scriptsList;
@@ -21,7 +18,7 @@ public class Area1_EntryCutScene : CutSceneBase
 	[Header("Skeleton Cut Scene")] 
 	[SerializeField] private List<SkeletonGraphic> skeletonList;
 	private Queue<SkeletonGraphic> skeletonQueue;
-
+	
 	protected override void Init()
 	{
 		skeletonQueue = new Queue<SkeletonGraphic>();
@@ -63,9 +60,9 @@ public class Area1_EntryCutScene : CutSceneBase
 		curScriptsIndex = (curScriptsIndex + 1 < scriptsList.Count) ? curScriptsIndex + 1 : 0;
 	}
 	
-	public void MovePlayer()
+	public void RandingPlayer()
 	{
-		chapterManager.PlayerController.LerpToWorldPosition(targetPos.transform.position, duration);
+		//playerAni.speed = 1.0f;
 	}
 
 	public void Area1_SpawnEnemy()
