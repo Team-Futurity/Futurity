ca02dd3d439cdcf33471a60ae9ab48b3e2c05a5e
Hyoung-Seok <dkavmdk98@gmail.com>
2023-11-07 14:25:45 +0900

    Refactor : CutSceneBase 구조 변경

--- Assets/00Scripts/Timeline/CutSceneBase.cs
+++ Assets/00Scripts/Timeline/CutSceneBase.cs
@@ -1,27 +1,90 @@
+using Spine.Unity;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public abstract class CutSceneBase : MonoBehaviour
 {
 	public ECutSceneType CutSceneType => cutSceneType;
+	
+	[Header("런타임 자동 초기화")]
+	[SerializeField, ReadOnly(false)] protected ChapterCutSceneManager chapterManager;
+	[SerializeField, ReadOnly(false)] protected PlayableDirector cutScene;
+	
+	[Space(3)]
+	[Header("컷씬 타입")]
 	[SerializeField] protected ECutSceneType cutSceneType;
-	[SerializeField] protected ChapterCutSceneManager chapterManager;
-	protected virtual void Init() { }
+
+	[Space(3)] [Header("스크립트 데이터")] 
+	[SerializeField] private bool isUseScripting;
+	[SerializeField] public List<ScriptingList> scriptingList;
+	protected int curScriptsIndex;
+
+	[Space(3)] 
+	[Header("스켈레톤 데이터")] 
+	public bool isUseSkeleton;
+	[SerializeField] protected Transform skeletonParent;
+	protected Queue<SkeletonGraphic> skeletonQueue;
+	
+	protected virtual void Init()
+	{
+		chapterManager = gameObject.GetComponentInParent<ChapterCutSceneManager>();
+		cutScene = gameObject.GetComponent<PlayableDirector>();
+		curScriptsIndex = 0;
+		
+		if (isUseSkeleton == true)
+		{
+			InitSkeletonQueue();
+		}
+	}
 	protected virtual void EnableCutScene() { }
 	protected abstract void DisableCutScene();
+	protected void StartScripting()
+	{
+		cutScene.Pause();
+		
+		chapterManager.PauseCutSceneUntilScriptsEnd(cutScene);
+		chapterManager.scripting.StartPrintingScript(scriptingList[curScriptsIndex].scriptList);
+		
+		curScriptsIndex = (curScriptsIndex + 1 < scriptingList.Count) ? curScriptsIndex + 1 : 0;
+	}
 
 	private void OnEnable()
 	{
-		chapterManager = gameObject.GetComponentInParent<ChapterCutSceneManager>();
-		InputActionManager.Instance.ToggleActionMap(InputActionManager.Instance.InputActions.UIBehaviour);
-		
 		Init();
 		EnableCutScene();
+		
+		InputActionManager.Instance.ToggleActionMap(InputActionManager.Instance.InputActions.UIBehaviour);
 	}
 
 	private void OnDisable()
 	{
 		InputActionManager.Instance.ToggleActionMap(InputActionManager.Instance.InputActions.Player);
 		DisableCutScene();
+
+		if (isUseScripting == false)
+		{
+			return;
+		}
+		
+		chapterManager.scripting.DisableAllNameObject();
+		chapterManager.scripting.ResetEmotion();
+	}
+
+	private void InitSkeletonQueue()
+	{
+		skeletonQueue = new Queue<SkeletonGraphic>();
+
+		for (int i = 0; i < skeletonParent.childCount; ++i)
+		{
+			if (skeletonParent.GetChild(i).TryGetComponent(out SkeletonGraphic cut) == false)
+			{
+				continue;
+			}
+			
+			skeletonQueue.Enqueue(cut);
+			cut.gameObject.SetActive(false);
+		}
 	}
 }
