d4d20219a0dc44cc99fb719fb94178d485252d52
Hyoung-Seok <dkavmdk98@gmail.com>
2023-11-07 10:40:16 +0900

    Remove : 삭제된 연출 스크립트 삭제

--- Assets/00Scripts/Timeline/ActiveParts/ActiveCutScene.cs
+++ Assets/00Scripts/Timeline/ActiveParts/ActiveCutScene.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Active_Alpha : CutSceneBase
+public class ActiveCutScene : CutSceneBase
 {
 	private Animator playerAnimator;
 
