ab81be20209422420b19edc10a42c6f909050c25
rbase0218 <rbase@naver.com>
2023-11-16 23:11:58 +0900

    Chore : Active Icon 추가

--- Assets/00Scripts/03UI/Button/Define.cs
+++ Assets/00Scripts/03UI/Button/Define.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public static class Define
 {
-	public static Color noneSelectColor = new Color(0f, 0f, 0f);
+	public static Color noneSelectColor = new Color(92f, 92f, 92f);
 	public static Color selectColor = new Color(255f, 255f, 255f);
 }
