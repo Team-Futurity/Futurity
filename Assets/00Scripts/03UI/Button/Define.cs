34af87b765da4ed07e5773f0eeefddbe936823c5
rbase0218 <rbase@naver.com>
2023-11-16 17:51:11 +0900

    FIx : 오탈자 수정

--- Assets/00Scripts/03UI/Button/Define.cs
+++ Assets/00Scripts/03UI/Button/Define.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public static class Define
 {
-	public static Color noneSelectcolor = new Color(0f, 0f, 0f);
+	public static Color noneSelectColor = new Color(0f, 0f, 0f);
 	public static Color selectColor = new Color(255f, 255f, 255f);
 }
