47ba9670ffdf6ba8ccd54699a80386c66b449ca1
rbase0218 <rbase@naver.com>
2023-11-17 00:23:27 +0900

    Fix : Loading 제거

--- Assets/00Scripts/Timeline/Boss/BossDeathCutScene.cs
+++ Assets/00Scripts/Timeline/Boss/BossDeathCutScene.cs
@@ -13,7 +13,7 @@ public class BossDeathCutScene : CutSceneBase
 
 	protected override void DisableCutScene()
 	{
-		SceneLoader.Instance.LoadScene("CreditScene");
+		SceneLoader.Instance.LoadScene("CreditScene", false);
 	}
 
 	public void BossDeath_StartSkeleton()
