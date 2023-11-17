15390893d9c97920fdb12c756b0d290b8a346aa3
Hyoung-Seok <dkavmdk98@gmail.com>
2023-11-17 14:52:00 +0900

    Fix : 씬 네임 저장 추가

--- Assets/00Scripts/ChapterMove/ChapterSceneName.cs
+++ Assets/00Scripts/ChapterMove/ChapterSceneName.cs
@@ -12,6 +12,7 @@ public static class ChapterSceneName
 {
 	public const string CHAPTER1_1 = "Chapter1-Stage1";
 	public const string CHAPTER1_2 = "Chapter1-Stage2";
+	public const string CHAPTER1_3 = "Chapter1-Stage3";
 	public const string CHAPTER2_1 = "Chapter2-Stage1";
 	public const string CHAPTER2_2 = "Chapter2-Stage2";
 	public const string BOSS_CHAPTER = "BossChapter";
