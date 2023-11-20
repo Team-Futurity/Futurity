c6db6e151cdb7980f699ee87b48dc1df0167cec1
Hyoung-Seok <dkavmdk98@gmail.com>
2023-11-20 21:43:23 +0900

    Remove : Dubug 관련 키 코드 삭제

--- Assets/00Scripts/00System/Tutorial/TutorialController.cs
+++ Assets/00Scripts/00System/Tutorial/TutorialController.cs
@@ -43,15 +43,7 @@ public class TutorialController : MonoBehaviour
 			combo.ChangeComboGauge(100);
 		});
 	}
-
-	private void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.F9))
-		{
-			SceneLoader.Instance.LoadScene(ChapterSceneName.CHAPTER1_1);
-		}
-	}
-
+	
 	private void SetEvent()
 	{
 		performHandler.CreateGroup(1);
